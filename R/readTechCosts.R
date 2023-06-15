464e92d482c3300c66f700cf06e8c9e633e8a270
Anastasis Giannousakis <giannousakis@e3modelling.com>
2023-06-15 12:14:38 +0300

    complete maritime subtype (TechCosts)

--- R/readTechCosts.R
+++ R/readTechCosts.R
@@ -24,6 +24,13 @@ readTechCosts <- function(subtype = "PowerAndHeat") { #nolint
     x <- pivot_longer(x, cols = grep("[a-z,A-Z]", names(x), invert = TRUE), names_to = "period") #nolint
     return(x)
 }
+.toolReadExcelChunk <- function(range, rangeRef) {
+
+      x <- .toolReadExcelWindow(file = file, sheet = sheet, range = range)
+      df <- read_excel(path = file, sheet = sheet, range = rangeRef)
+      if (grepl("Capital cost", df[, 1])) x <- rbind(.toolAddRef(x, df), x)
+      return(x)
+}
 
 
   if (subtype == "PowerAndHeat") {
@@ -54,19 +61,23 @@ readTechCosts <- function(subtype = "PowerAndHeat") { #nolint
       file <- "REF2020_Technology Assumptions_Transport.xlsx"
       sheet <- "Maritime"
 
-      range <- "B10:F20"
-      x <- .toolReadExcelWindow(file = file, sheet = sheet, range = range)
-      range <- "B6:D8"
-      df <- read_excel(path = file, sheet = sheet, range = range)
-      if (grepl("Capital cost", df[, 1])) x <- rbind(.toolAddRef(x, df), x)
-
-      range <- "B29:F39"
-      tmp <- .toolReadExcelWindow(file = file, sheet = sheet, range = range)
-      x <- rbind(x, tmp)
-      range <- "B25:D27"
-      df <- read_excel(path = file, sheet = sheet, range = range)
-      if (grepl("Capital cost", df[, 1])) x <- rbind(.toolAddRef(tmp, df), x)
-
+      x <- rbind(.toolReadExcelChunk("B10:F20", "B6:D8"), # diesel/fuel oil  Container 1000-1999 TEU vessel
+                 .toolReadExcelChunk("B29:F39", "B25:D27"), # LNG Container 1000-1999 TEU vessel
+                 .toolReadExcelChunk("B47:E57", "B44:D45"), # Fuel Cell  Container 1000-1999 TEU vessel
+                 .toolReadExcelChunk("B65:E71", "B62:D63"), # Electric  Container 1000-1999 TEU vessel
+                 .toolReadExcelChunk("B81:F91", "B77:D79"), # diesel/fuel oil  handymax-supra bulk carrier vessel
+                 .toolReadExcelChunk("B100:F110", "B96:D98"), # LNG  handymax-supra bulk carrier vessel
+                 .toolReadExcelChunk("B118:E128", "B115:D116"), # Fuel Cell handymax-supra bulk carrier vessel
+                 .toolReadExcelChunk("B136:E142", "B133:D134"), # Electric handymax-supra bulk carrier vessel
+                 .toolReadExcelChunk("B152:F162", "B148:D150"), # diesel/fuel oil General cargo vessel
+                 .toolReadExcelChunk("B171:F181", "B167:D169"), # LNG General cargo vessel
+                 .toolReadExcelChunk("B189:E199", "B186:D187"), # Fuel Cell General cargo vessel
+                 .toolReadExcelChunk("B207:E213", "B204:D205"), # Electric General cargo vessel
+                 .toolReadExcelChunk("B223:F233", "B219:D221"), # diesel/fuel oil  Tanker 10000-69999 DWT
+                 .toolReadExcelChunk("B242:F252", "B238:D240"), # LNG  Tanker 10000-69999 DWT
+                 .toolReadExcelChunk("B260:E270", "B257:D258"), # Fuel Cell  Tanker 10000-69999 DWT
+                 .toolReadExcelChunk("B278:E284", "B275:D276") # Fuel Cell  Tanker 10000-69999 DWT
+                 )
 
       x[["value"]] <- as.numeric(x[["value"]])
       x[["unit"]] <- sub("^.*. in ", "", x[["variable"]])
@@ -75,5 +86,5 @@ readTechCosts <- function(subtype = "PowerAndHeat") { #nolint
       x <- as.quitte(x)
   }
 
-return(as.magpie(x))
+return(suppressWarnings(as.magpie(x)))
 }
