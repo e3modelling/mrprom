60e99d4e3087bba9baa8ce2d7e881d5764e3f273
Fotis Sioutas <Sioutas@internal.e3modelling.gr>
2023-06-26 12:54:23 +0300

    add function readIRF

---
+++ R/readIRF.R
@@ -0,0 +1,73 @@
+#' readIRF
+#'
+#' Read in an csv file and convert it to a magpie object.
+#' The dataset contains several data types about transport and traffic.
+#' The data is from https://datawarehouse.worldroadstatistics.org/users/login
+#'
+#' @param subtype By choosing a subtype you can select the corresponding csv
+#' file and convert it to a magpie object.
+#' Available types are:
+#' \itemize{
+#' \item `passenger-cars-in-use`:
+#' \item `buses-and-motor-coaches-in-use`:
+#' \item `bus-and-motor-coach-traffic`:
+#' \item `lorry-and-road-tractor-traffic`:
+#' \item `inland-surface-freight-transport-by-inland-waterway`:
+#' \item `inland-surface-freight-transport-by-rail`:
+#' \item `inland-surface-freight-transport-by-road`:
+#' \item `inland-surface-passenger-transport-by-rail`:
+#' \item `inland-surface-public-passenger-transport-by-road`:
+#' }
+#'
+#' @return The read-in data into a magpie object
+#'
+#' @author Anastasis Giannousakis, Fotis Sioutas
+#'
+#' #' @examples
+#' \dontrun{
+#' a <- readSource("IRF")
+#' }
+#'
+#' @importFrom quitte as.quitte
+#' @importFrom utils read.csv
+#'
+readIRF <- function(subtype = "passenger-cars-in-use") {
+
+
+  x <- read.csv(file = paste0(subtype, ".csv"), header = FALSE)
+  names(x) <- toolCountry2isocode(as.character(x[1, ]), mapping = c("China, Hong Kong" = "HKG",
+                                                                    "China, Macao" = "MAC",
+                                                                    "Egypt, Arab Rep." = "EGY",
+                                                                    "Korea, Rep." = "KOR",
+                                                                    "Micronesia, Fed. Sts." = "FSM",
+                                                                    "St. Helena" = "SHN",
+                                                                    "St. Kitts and Nevis" = "KNA",
+                                                                    "St. Lucia" = "LCA",
+                                                                    "St. Vincent and the Grenadines" = "VCT",
+                                                                    "Venezuela, RB" = "VEN",
+                                                                    "Yemen, Rep." = "YEM",
+                                                                    "Congo, Dem. Rep." = "COD",
+                                                                    "Iran, Islamic Rep." = "IRN",
+                                                                    "Category" = "NA"))
+  x <- x[c(2:nrow(x)), ]
+  names(x)[1] <- "period"
+  x <- gather(x, region, value,  grep("[A-Z][A-Z][A-Z]", names(x), value = TRUE))
+  x[["variable"]] <- subtype
+  if (subtype %in% c("passenger-cars-in-use",
+                     "buses-and-motor-coaches-in-use")) {
+    x[["unit"]] <- "veh"
+  } else if (subtype %in% c("bus-and-motor-coach-traffic",
+                            "lorry-and-road-tractor-traffic")) {
+    x[["unit"]] <- "km/yr"
+  } else if (subtype %in% c("inland-surface-freight-transport-by-inland-waterway",
+                            "inland-surface-freight-transport-by-rail",
+                            "inland-surface-freight-transport-by-road")) {
+    x[["unit"]] <- "million tKm/yr"
+  } else if (subtype %in% c("inland-surface-passenger-transport-by-rail",
+                            "inland-surface-public-passenger-transport-by-road")) {
+    x[["unit"]] <- "million pKm/yr"
+  }
+  x[["value"]] <- as.numeric(x[["value"]])
+  return(as.magpie(as.quitte(x)))
+
+}
