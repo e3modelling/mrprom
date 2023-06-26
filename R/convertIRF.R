e90078f0f62dc713b9d49953eaac1ef443f1a588
Fotis Sioutas <Sioutas@internal.e3modelling.gr>
2023-06-26 13:43:50 +0300

    add function convertIRF

---
+++ R/convertIRF.R
@@ -0,0 +1,29 @@
+#' convertIRF
+#'
+#' Add missing periods to data frame and interpolate missing values.
+#' The ISO codes of "IRF" data are compared with the official ISO code country list.
+#' NA values are replaced with zeros and historical data are corrected.
+#'
+#' @param x MAgPIE object.
+#'
+#' @return The "IRF" data with spatial entries for each country.
+#'
+#' @author Anastasis Giannousakis, Fotis Sioutas
+#'
+#' @examples
+#' \dontrun{
+#' a <- readSource("IRF", convert = TRUE)
+#' }
+#'
+#' @importFrom quitte as.quitte
+#' @importFrom quitte interpolate_missing_periods
+
+convertIRF <- function(x) {
+
+  x <- as.quitte(x) %>%
+    interpolate_missing_periods(period = seq(2015, 2020, 1), expand.values = TRUE) %>%
+    as.magpie()
+  x <- toolCountryFill(x, fill = 0) #nolint
+  return(x[as.character(getISOlist()), , ]) #nolint
+
+}
