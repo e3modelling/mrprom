7be078b211167bc51713a6c9eaab8e6e67d7e9bc
Fotis Sioutas <Sioutas@internal.e3modelling.gr>
2023-07-04 12:21:39 +0300

    add function convertEurostat2

---
+++ R/convertEurostat2.R
@@ -0,0 +1,37 @@
+#' convertEurostat2
+#'
+#' @param x MAgPIE object.
+#'
+#' @return The "EUROSTAT" data with spatial entries for each country.
+#'
+#' @author Anastasis Giannousakis, Fotis Sioutas
+#'
+#' @examples
+#' \dontrun{
+#' a <- readSource("Eurostat2", convert = TRUE)
+#' }
+#'
+#' @importFrom quitte as.quitte
+#' @importFrom dplyr select
+#'
+
+convertEurostat2 <- function(x) {
+
+  x <- as.quitte(x)
+  x <- as.data.frame(x)
+  x <- subset(x, select=-c(region))
+  names(x) <- sub("geo","region", names(x))
+
+  x[["region"]] <- toolCountry2isocode(x[["region"]],
+                                       mapping = c("EL" = "GRC",
+                                                   "XK" = "KOS"))
+
+
+  x <- x[!is.na(x$region), ]
+  x <- x[!is.na(x$value), ]
+  x <- as.quitte(x)
+  x <- as.magpie(x)
+  x <- toolCountryFill(x, fill = 0)
+
+  return(x[as.character(getISOlist()), , ])
+}
