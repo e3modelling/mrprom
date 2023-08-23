b6e12d9484187c5d02758f965cace39c41a176cb
Anastasis Giannousakis <Giannousakis@e3modelling.com>
2023-08-23 19:02:24 +0300

    mapping for enerdata -> openprom names conversion

--- R/calcIFinConsSubFuel.R
+++ R/calcIFinConsSubFuel.R
@@ -22,8 +22,11 @@ calcIFinConsSubFuel <- function() {
   nen <- calcOutput("FuelConsumption", subtype = "non energy", aggregate = FALSE)
   bun <- calcOutput("FuelConsumption", subtype = "bunkers", aggregate = FALSE)
   pch <- calcOutput("FuelConsumption", subtype = "chemical feedstock", aggregate = FALSE)
-
-
+  x <- mbind(nen, bun, pch)
+  map <- toolGetMapping("prom-enerdata-fucon-mapping.csv", "sectoral")
+  x <- x[, , map[!is.na(map[, 1]), 1]]
+  getNames(x)<-paste0(paste(map[c(3,4,7),2],map[c(3,4,7),3],sep="."),".",sub("^.*.\\..*.\\.","",getNames(tmp)))
+  #duplicated(sub("\\..*.$","",getNames(x))) CHECK MISSING DATA due to different units
   x <-as.quitte(x)
   x <- as.magpie(x)
   u <- getItems(x, "unit")
