e6516e92519cd8d16985d05c31e1886648b97c35
Anastasis Giannousakis <Giannousakis@e3modelling.com>
2023-09-29 08:33:48 +0200

    more complete ACTV data

--- R/calcACTV.R
+++ R/calcACTV.R
@@ -81,8 +81,34 @@ calcACTV <- function() {
   levels(tr[["variable"]]) <- sub("lorry-and-road-tractor-traffic", "GU", levels(tr[["variable"]]))
   levels(tr[["variable"]]) <- sub("inland-surface-freight-transport-by-road", "GT", levels(tr[["variable"]]))
   levels(tr[["variable"]]) <- sub("inland-surface-freight-transport-by-inland-waterway", "GN", levels(tr[["variable"]])) # nolint
-  x <- rbind(as.quitte(x), filter(tr, tr[["region"]] %in% getRegions(x)))
-  x <- as.magpie(x)
+  qx <- rbind(as.quitte(x), filter(tr, tr[["region"]] %in% getRegions(x)))
+
+
+  # assign to countries with NA, their H12 region mean
+  qx_bu <- qx
+  h12 <- toolGetMapping("regionmappingH12.csv")
+  names(qx) <- sub("region", "CountryCode", names(qx))
+  ## add h12 mapping to dataset
+  qx <- left_join(qx, h12, by="CountryCode")
+  ## add new column containing regional mean value
+  value <- NULL
+  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable"))
+  names(qx) <- sub("CountryCode", "region", names(qx))
+  qx <- select(qx, -c("model", "scenario", "X", "RegionCode"))
+  qx_bu <- select(qx_bu, -c("model", "scenario"))
+  ## assign to countries with NA, their H12 region mean
+  value.x <- NULL
+  value.y <- NULL
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>% 
+         mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>% 
+         select(-c("value.x", "value.y"))
+  ## assign to countries that still have NA, the global mean
+  qx_bu <- qx
+  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("period", "variable"))
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>% 
+         mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>% 
+         select(-c("value.x", "value.y"))
+  x <- as.quitte(qx) %>% as.magpie()
   getNames(x) <- sub("\\..*$", "", getNames(x)) # remove units in the file read by GAMS
 
   list(x = x,
