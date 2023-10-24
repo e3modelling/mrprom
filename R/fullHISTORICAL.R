2b89c9535ca68a09097d1e0050eaacf1283afea2
Fsioutas <fsioutas@hotmail.com>
2023-10-24 15:07:42 +0300

    add function readMENA_EDS

--- R/fullHISTORICAL.R
+++ R/fullHISTORICAL.R
@@ -1,6 +1,6 @@
 #' fullHISTORICAL
 #'
-#' Read files, convert it to a mif file so to compare mrprom output mif file
+#' Read data, convert it to a mrprom mif file so to compare output mif file
 #' with OPEN-PROM output.
 #'
 #' @return The mif file
@@ -9,82 +9,45 @@
 #'
 #' @examples
 #' \dontrun{
-#' a <- retrieveData("Historical", regionmapping = "regionmappingOPDEV2.csv")
+#' a <- retrieveData("HISTORICAL", regionmapping = "regionmappingOPDEV2.csv")
 #' }
 #'
 #' @importFrom quitte as.quitte write.mif
-#' @importFrom dplyr select left_join filter
-#' @importFrom stringr word
+#' @importFrom dplyr select
 #'
 #' @export
 
 fullHISTORICAL <- function() {
   
   y <- NULL
-  variable2 <- NULL
   variable <- NULL
-  variable3 <- NULL
-  SBS <- NULL
-  EF <- NULL
-  k <- NULL
-  
-  z <- readSource("ENERDATA", "consumption", convert = TRUE)
-  
-  for (i in c("NENSE", "DOMSE", "INDSE","TRANSE")) {
-
-    # filter years
-    fStartHorizon <- readEvalGlobal(system.file(file.path("extdata", "main.gms"), package = "mrprom"))["fStartHorizon"]
-    fStartY <- readEvalGlobal(system.file(file.path("extdata", "main.gms"), package = "mrprom"))["fStartY"]
-    x <- z[, c(fStartHorizon:fStartY), ]
-    
-    # load current OPENPROM set configuration
-    sets <- readSets(system.file(file.path("extdata", "sets.gms"), package = "mrprom"), i)
-    sets <- unlist(strsplit(sets[,1],","))
-    
-    # use enerdata-openprom mapping to extract correct data from source
-    map <- toolGetMapping(name = "prom-enerdata-fucon-mapping.csv",
-                          type = "sectoral",
-                          where = "mappingfolder")
-    
-    ## filter mapping to keep only XXX sectors
-    map <- filter(map, map[, "SBS"] %in% sets)
-    ## ..and only items that have an enerdata-prom mapping
-    enernames <- unique(map[!is.na(map[, "ENERDATA"]), "ENERDATA"])
-    map <- map[map[, "ENERDATA"] %in% enernames, ]
-    ## filter data to keep only XXX data
-    enernames <- unique(map[!is.na(map[, "ENERDATA"]), "ENERDATA"])
-    x <- x[, , enernames]
-  
+  new <- NULL
+  data <- NULL
+  for (i in c("TRANSE")) {
+    x <- calcOutput(type = "IFuelCons", subtype = i, aggregate = TRUE)
     x[is.na(x)] <- 0
     xq <- as.quitte(x)
-    map[, 1] <- word(map[, 1], sep = fixed("."), 1)
-    xq <- left_join(xq, map, by=c( "variable" = "ENERDATA"))
-    
-    xq["variable2"] <- "fuel consumption"
-    xq["variable3"] <- i
-    xq$variable <- paste(xq$variable2, xq$SBS, xq$EF, xq$variable3)
-    xq <- select((xq), -c(SBS, EF, variable2, variable3))
+    xq$variable <- paste(xq$variable, xq$new)
+    xq <- select((xq), -c(new))
     xq["model"] <- "ENERDATA"
     y <- rbind(y, xq)
   }
-  y <- as.quitte(y)
-  #write.mif(y, 'Data_validation_mrprom.mif', append = FALSE)
   
-  
-  gdp <- calcOutput("iGDP", aggregate = FALSE)
+  gdp <- calcOutput("iGDP", aggregate = TRUE)
   q1 <- as.quitte(gdp)
   q1["variable"] <- "GDP|PPP"
   q1["unit"] <- "billion US$2015/yr"
-  POP <- calcOutput("POP", aggregate = FALSE)
+  q1["model"] <- "SSP"
+  
+  POP <- calcOutput("POP", aggregate = TRUE)
   q2 <- as.quitte(POP)
   q2["variable"] <- "Population"
   q2["unit"] <- "billion"
   q2["model"] <- "SSP"
-  q1["model"] <- "SSP"
-  
   q <- rbind(q1, q2)
   q <- select((q), -c(data))
+  
   z <- rbind(y, q)
-  write.mif(z, 'GDP_POP_CONSUMPTION.mif', append = FALSE)
+  write.mif(z, 'mrprom.mif', append = FALSE)
   
 }
--- R/readMENA_EDS.R
+++ R/readMENA_EDS.R
@@ -1,6 +1,6 @@
 #' readMENA_EDS
 #'
-#' Read gdx files, convert it to a mif file so to compare MENA_EDS output mif file
+#' Read MENA_EDS gdx files, convert it to a MENA_EDS mif file so to compare output mif file
 #' with OPEN-PROM output.
 #'
 #' @return The read-in data into a mif object.
@@ -19,38 +19,16 @@
 
 readMENA_EDS <- function() { 
   
-  x <- readGDX(gdx = "fulldata.gdx", name = c("Nen_FCon", "Dom_FCon", "Indu_FCon",
-                                              "Trans_FCon", "iGDP", "POP"))
-  Nen_FCon <- readGDX(gdx = "fulldata.gdx", name = c("Nen_FCon"), field = "l")
-  Nen_FCon <- as.quitte(Nen_FCon)
-  Nen_FCon["model"] <- "MENA_EDS"
-  Nen_FCon["variable"] <- paste("fuel consumption", Nen_FCon$NENSE, Nen_FCon$EF, "NENSE")
-  Nen_FCon <- select((Nen_FCon), -c(NENSE, EF))
-  
-  Dom_FCon <- readGDX(gdx = "fulldata.gdx", name = c("Dom_FCon"), field = "l")
-  Dom_FCon <- as.quitte(Dom_FCon)
-  Dom_FCon["model"] <- "MENA_EDS"
-  Dom_FCon["variable"] <- paste("fuel consumption", Dom_FCon$DOMSE, Dom_FCon$EF, "DOMSE")
-  Dom_FCon <- select((Dom_FCon), -c(DOMSE, EF))
-  
-  Indu_FCon <- readGDX(gdx = "fulldata.gdx", name = c("Indu_FCon"), field = "l")
-  Indu_FCon <- as.quitte(Indu_FCon)
-  Indu_FCon["model"] <- "MENA_EDS"
-  Indu_FCon["variable"] <- paste("fuel consumption", Indu_FCon$INDSE, Indu_FCon$EF, "INDSE")
-  Indu_FCon <- select((Indu_FCon), -c(INDSE, EF))
-  
-  Trans_FCon <- readGDX(gdx = "fulldata.gdx", name = c("Trans_FCon"), field = "l")
-  Trans_FCon <- as.quitte(Trans_FCon)
-  Trans_FCon["model"] <- "MENA_EDS"
-  Trans_FCon["variable"] <- paste("fuel consumption", Trans_FCon$TRANSE, Trans_FCon$EF, "TRANSE")
-  Trans_FCon <- select((Trans_FCon), -c(TRANSE, EF))
-  
-
-  
-  y <- rbind(Nen_FCon, Dom_FCon, Indu_FCon, Trans_FCon)
-
-  y <- as.quitte(y)
-  y["unit"] <- "Mtoe"
+  TRANSE <- NULL
+  EF <- NULL
+  data <- NULL
+  #TRANSE
+  DEMTR <- readGDX(gdx = "fulldata.gdx", name = c("DEMTR"), field = "l")
+  DEMTR <- as.quitte(DEMTR)
+  DEMTR["model"] <- "MENA_EDS"
+  DEMTR["variable"] <- paste(DEMTR$TRANSE, DEMTR$EF)
+  DEMTR <- select((DEMTR), -c(TRANSE, EF))
+  DEMTR["unit"] <- "Mtoe"
   
   iGDP <- readGDX(gdx = "fulldata.gdx", name = c("GDP"), field = "l")
   q1 <- as.quitte(iGDP)
@@ -68,8 +46,8 @@ readMENA_EDS <- function() {
   q <- rbind(q1, q2)
   q <- select((q), -c(data))
 
-  z <- rbind(y, q)
-  write.mif(z, 'GDP_POP_CONSUMPTION_MENA_EDS.mif', append = FALSE)
+  z <- rbind(DEMTR, q)
+  write.mif(z, 'MENA_EDS.mif', append = FALSE)
 
   
   return(suppressWarnings(as.magpie(z)))
