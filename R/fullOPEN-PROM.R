47856d811ce8b3602846802bdda92ad52fd85e7c
derevirn <derevirn@gmail.com>
2023-11-22 22:11:07 +0200

    Added func calcIDataImports

--- R/fullOPEN-PROM.R
+++ R/fullOPEN-PROM.R
@@ -201,6 +201,21 @@ fullOPEN_PROM <- function() {
               sep = ",",
               col.names = FALSE,
               append = TRUE)
+
+  x <- calcOutput("IDataImports", aggregate = TRUE)
+  xq <- as.quitte(x) %>%
+    select(c("region", "variable", "period", "value")) %>%
+    pivot_wider(names_from = "period")
+  fheader <- paste("dummy,dummy", paste(colnames(xq)[3 : length(colnames(xq))], collapse = ","), sep = ",")
+  writeLines(fheader, con = "iDataImports.csv")
+  write.table(xq,
+              quote = FALSE,
+              row.names = FALSE,
+              file = "iDataImports.csv",
+              sep = ",",
+              col.names = FALSE,
+              append = TRUE)
+  
   
   return(list(x = x,
               weight = NULL,
