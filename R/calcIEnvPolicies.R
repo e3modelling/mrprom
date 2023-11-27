5267d4afa6e1be4c6d95cac08efd24417de4ef95
Fsioutas <fsioutas@hotmail.com>
2023-11-27 18:06:37 +0200

    added functions, calcIEnvPolicies, readEU_RefScen2020, convertEU_RefScen2020

---
+++ R/calcIEnvPolicies.R
@@ -0,0 +1,74 @@
+#' calcIEnvPolicies
+#'
+#' Use carbon price data from the EU Reference Scenario 2020 and from the
+#' policies scenario GP_CurPol_T45 to derive OPENPROM input parameter
+#' iEnvPolicies.
+#'
+#' @return  OPENPROM input data iEnvPolicies.
+#' The output data when overlapping between EU Reference Scenario 2020 and 
+#' policies scenario GP_CurPol_T45 takes the EU Reference Scenario 2020 value.
+#'
+#' @author Anastasis Giannousakis, Fotis Sioutas, Giannis Tolios
+#'
+#' @examples
+#' \dontrun{
+#' a <- calcOutput(type = "IEnvPolicies", aggregate = FALSE)
+#' }
+#'
+#' @importFrom dplyr left_join %>% filter select mutate
+#' @importFrom tidyr expand_grid
+#' @importFrom quitte as.quitte interpolate_missing_periods
+
+calcIEnvPolicies <- function() {
+  
+  a1 <- readSource("EU_RefScen2020")
+  a2 <- readSource("ENGAGE")
+  
+  q1 <- as.quitte(a1) %>%
+    interpolate_missing_periods(period = getYears(a2, as.integer = TRUE), expand.values = FALSE)
+  q2 <- as.quitte(a2)
+  q2 <- mutate(q2, value = mean(value, na.rm = TRUE), .by = c("region", "scenario", "unit", "period", "variable"))
+  q2["model"] <- "Average of scenario GP_CurPol_T45"
+  q2 <- q2 %>% distinct()
+  
+  qx <- left_join(q2, q1, by = c("region", "period", "variable"))
+  
+  qx[["value.x"]] <- ifelse(!is.na(qx[["value.y"]]) & (qx[["value.y"]]) > 0, qx[["value.y"]], qx[["value.x"]])
+  qx[["model.x"]] <- ifelse(!is.na(qx[["value.y"]]) & (qx[["value.y"]]) > 0, as.character(qx[["model.y"]]), as.character(qx[["model.x"]]))
+  qx[["scenario.x"]] <- ifelse(!is.na(qx[["value.y"]]) & (qx[["value.y"]]) > 0, as.character(qx[["scenario.y"]]), as.character(qx[["scenario.x"]]))
+  qx[["unit.x"]] <- ifelse(!is.na(qx[["value.y"]]) & (qx[["value.y"]]) > 0, as.character(qx[["unit.y"]]), as.character(qx[["unit.x"]]))
+ 
+  qx <- select(qx, -c("model.y", "scenario.y", "unit.y", "value.y"))
+  names(qx)[1] <- "model"
+  names(qx)[2] <- "scenario"
+  names(qx)[5] <- "unit"
+  names(qx)[7] <- "value"
+  
+  qx["scenario"] <- NA
+  qx["model"] <- NA
+  qx["unit"] <- NA
+  
+  qx <- as.quitte(qx)
+  
+  qx <- interpolate_missing_periods(qx, 2010:2100, expand.values = TRUE)
+  
+  qx <- filter(qx, period >= 2010)
+  
+  
+  # load current OPENPROM set configuration
+  POLICIES_set <- readSets(system.file(file.path("extdata", "sets.gms"), package = "mrprom"), "POLICIES_set")
+  POLICIES_set <- unlist(strsplit(POLICIES_set[, 1], ","))
+  POLICIES_set <- POLICIES_set[c(1,3:5,11)]
+  
+  x <- expand_grid(POLICIES_set, unique(qx["region"]), unique(qx["period"]))
+  x["value"] <- NA
+  x[x["POLICIES_set"] == "exogCV", 4] <- qx["value"]
+  
+  x <- as.quitte(x) %>% as.magpie()
+  
+  list(x = x,
+       weight = NULL,
+       unit = "various",
+       description = "Carbon price data from EU Reference Scenario 2020 and policies scenario GP_CurPol_T45")
+}
+  
---
+++ R/readEU_RefScen2020.R
@@ -0,0 +1,27 @@
+#' readEU_RefScen2020
+#'
+#' Read in data from CarbonPrice_fromReportFig8.
+#' The dataset contains carbon price data for the EU Reference Scenario 2020.
+#'
+#' @return The read-in carbon price data into a magpie object
+#'
+#' @author Anastasis Giannousakis
+#'
+#' @examples
+#' \dontrun{
+#' a <- readSource("EU_RefScen2020", convert = TRUE)
+#' }
+#'
+#' @importFrom utils read.csv
+#'
+readEU_RefScen2020 <- function() {
+  
+  x <- read.csv(file = "CarbonPrice_fromReportFig8.csv")
+  
+  names(x) <- sub("X", "", names(x))
+
+  x <- as.magpie(x)
+  getSets(x)[2] <- "Year"
+  return(x)
+  
+}
