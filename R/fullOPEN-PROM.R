c89f0e8a1a98c1cca8d6e4a7318a138bacf3a4bb
Anastasis Giannousakis <giannousakis@e3modelling.com>
2023-11-30 19:01:12 +0200

    add function and add it to fullOPENPROM

--- R/fullOPEN-PROM.R
+++ R/fullOPEN-PROM.R
@@ -259,6 +259,21 @@ fullOPEN_PROM <- function() {
               sep = ",",
               col.names = FALSE,
               append = TRUE)
+
+
+  x <- calcOutput(type = "IDataElecProd", aggregate = TRUE)
+    xq <- as.quitte(x) %>%
+    select(c("region", "variable", "period", "value")) %>%
+    pivot_wider(names_from = "period")
+  fheader <- paste("dummy,dummy", paste(colnames(xq)[3 : length(colnames(xq))], collapse = ","), sep = ",")
+  writeLines(fheader, con = "iDataElecProd.csv")
+  write.table(xq,
+              quote = FALSE,
+              row.names = FALSE,
+              file = "iDataElecProd.csv",
+              sep = ",",
+              col.names = FALSE,
+              append = TRUE)
   
   return(list(x = x,
               weight = NULL,
