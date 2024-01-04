a7f52cd4babd97110c2bbdab54b30bb60d1d76e6
derevirn <derevirn@gmail.com>
2024-01-04 23:25:29 +0200

    Added calcISuppRefCapacity to fullOPEN-PROM

--- R/fullOPEN-PROM.R
+++ R/fullOPEN-PROM.R
@@ -431,6 +431,20 @@ fullOPEN_PROM <- function() {
               col.names = FALSE,
               append = TRUE)
   
+  x <- calcOutput(type = "ISuppRefCapacity", aggregate = TRUE)
+  xq <- as.quitte(x) %>%
+    select(c("region", "variable", "period", "value")) %>%
+    pivot_wider(names_from = "period")
+  fheader <- paste("dummy,dummy", paste(colnames(xq)[3 : length(colnames(xq))], collapse = ","), sep = ",")
+  writeLines(fheader, con = "iSuppRefCapacity.csv")
+  write.table(xq,
+              quote = FALSE,
+              row.names = FALSE,
+              file = "iSuppRefCapacity.csv",
+              sep = ",",
+              col.names = FALSE,
+              append = TRUE)
+  
   return(list(x = x,
               weight = NULL,
               unit = "various",
