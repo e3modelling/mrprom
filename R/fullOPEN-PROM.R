bda8399639d0c0e1bd662709e76dcd3b8f84f1a8
derevirn <derevirn@gmail.com>
2024-01-09 23:36:07 +0200

    Added calc function to fullOPEN-PROM

--- R/fullOPEN-PROM.R
+++ R/fullOPEN-PROM.R
@@ -473,6 +473,21 @@ fullOPEN_PROM <- function() {
               col.names = FALSE,
               append = TRUE)
   
+  x <- calcOutput(type = "IDataGrossInlCons", aggregate = TRUE)
+  xq <- as.quitte(x) %>%
+    select(c("region", "variable", "period", "value")) %>%
+    pivot_wider(names_from = "period")
+  fheader <- paste("dummy,dummy", paste(colnames(xq)[3 : length(colnames(xq))], collapse = ","), sep = ",")
+  writeLines(fheader, con = "iDataGrossInlCons.csv")
+  write.table(xq,
+              quote = FALSE,
+              row.names = FALSE,
+              file = "iDataGrossInlCons.csv",
+              sep = ",",
+              col.names = FALSE,
+              append = TRUE)
+  
+  
   return(list(x = x,
               weight = NULL,
               unit = "various",
