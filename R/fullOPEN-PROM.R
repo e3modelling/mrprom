812e1a0fbcf83e83cf0522ee65a8137cd0631248
Anastasis Giannousakis <giannousakis@e3modelling.com>
2024-01-10 16:14:30 +0200

    create function iInstCapPast and add to fullOPEN-PROM

--- R/fullOPEN-PROM.R
+++ R/fullOPEN-PROM.R
@@ -416,6 +416,20 @@ fullOPEN_PROM <- function() {
               sep = ",",
               col.names = FALSE,
               append = TRUE)
+
+  x <- calcOutput("IInstCapPast", aggregate = TRUE)
+  xq <- as.quitte(x) %>%
+    select(c("period", "value", "region", "variable")) %>%
+    pivot_wider(names_from = "period")
+  fheader <- paste("dummy,dummy", paste(colnames(xq)[3 : length(colnames(xq))], collapse = ","), sep = ",")
+  writeLines(fheader, con = "iInstCapPast.csv")
+  write.table(xq,
+              quote = FALSE,
+              row.names = FALSE,
+              file = "iInstCapPast.csv",
+              sep = ",",
+              col.names = FALSE,
+              append = TRUE)
   
   x <- calcOutput(type = "ISuppPrimprod", aggregate = TRUE)
   xq <- as.quitte(x) %>%
