7558e071a8252233c38cf37b5151a1fd668b1d5d
Fsioutas <fsioutas@hotmail.com>
2024-06-11 14:42:57 +0300

    add excel to source SSP update mrdrivers

--- R/calciGDP.R
+++ R/calciGDP.R
@@ -20,9 +20,8 @@ calciGDP <- function(scenario = "SSP2") {
 
 
   x <- readSource("SSP", "gdp", convert = TRUE)
-  getSets(x) <- c("region", "period", "model", "scenario", "variable", "unit")
   x <- as.quitte(x[, , scenario]) %>% interpolate_missing_periods(period = seq(2010, 2100, 1))
-  x["value"] <- x["value"] * (1.2136) # convert US$2005 to 2015
+  x["value"] <- x["value"] * (0.97) # convert US$2017 to 2015
   list(x = collapseNames(as.magpie(x)),
        weight = NULL,
        unit = "billion US$2015/yr",
