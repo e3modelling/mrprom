7ae8d34585cbec78d2b00991e2f5ade36ba4ecdd
Fsioutas <fsioutas@hotmail.com>
2024-12-18 11:13:57 +0200

    add regional or global mean to countries with NA calcUNFCCC_IP_CO2

--- R/calcUNFCCC_IP_CO2.R
+++ R/calcUNFCCC_IP_CO2.R
@@ -46,7 +46,36 @@ calcUNFCCC_IP_CO2 <- function() {
   qx[["variable"]] <- qx[["category"]]
   qx <- select(qx, -"category")
   qx[["unit"]] <- "Mt CO2/yr"
-  qx[["value"]] <- qx[["value"]] / 1000
+  qx[["value"]] <- qx[["value"]] / 1000 #from kt to Mt CO2/yr
+  
+  # Assign to countries with NA, their H12 region mean
+  qx_bu <- qx
+  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
+  names(qx) <- sub("region", "CountryCode", names(qx))
+  
+  ## Add h12 mapping to dataset
+  qx <- left_join(qx, h12, by = "CountryCode")
+  
+  ## Add new column containing regional mean value
+  value <- NULL
+  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable"))
+  names(qx) <- sub("CountryCode", "region", names(qx))
+  qx <- select(qx, -c("model", "scenario", "X", "RegionCode"))
+  qx_bu <- select(qx_bu, -c("model", "scenario"))
+  
+  ## Assign the H12 region mean where necessary
+  value.x <- NULL
+  value.y <- NULL
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
+    select(-c("value.x", "value.y"))
+  
+  ## Assign to countries that still have NA, the global mean
+  qx_bu <- qx
+  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("period", "variable"))
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
+    select(-c("value.x", "value.y"))
 
   x <- as.quitte(qx) %>% as.magpie()
   x[is.na(x)] <- 0
