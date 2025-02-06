0a5d2ff087bf03cc7e4eeedab81989a4c709d635
Fsioutas <fsioutas@hotmail.com>
2025-02-06 17:03:02 +0200

    fix function toolFunnelPlots

--- R/toolFunnelPlots.R
+++ R/toolFunnelPlots.R
@@ -1,6 +1,7 @@
 #' toolFunnelPlots
 #'
-#' Make FunnelPlots from quitte object for specific years, regions and variables.
+#' Make FunnelPlots from magpie object for specific regions and variables
+#' with many models and scenarios and years.
 #'
 #' @param subtypes magpie object
 #'
@@ -10,11 +11,12 @@
 #'
 #' @examples
 #' \dontrun{
-#' a <- toolFunnelPlots(subtype = df)
+#' a <- toolFunnelPlots(subtype = x)
 #' }
 #'
 #' @importFrom dplyr filter %>% mutate select
 #' @importFrom quitte as.quitte interpolate_missing_periods
+#' @importFrom tidyr drop_na
 #' @import ggplot2
 #'
 #' @export
@@ -22,49 +24,41 @@
 toolFunnelPlots <- function(subtypes = x) {
   
   df <- as.quitte(x)
+  df <- drop_na(df)
+  model <- df["model"]
+  names(model) <- "temp"
+  df[["model"]] <- paste0(df[["model"]], ".", df[["scenario"]])
+  df <- cbind(df, model)
+  statistics <- mutate(df, max = max(value, na.rm = TRUE), .by = c("region", "period"))
+  statistics <- mutate(statistics, min = min(value, na.rm = TRUE), .by = c("region", "period"))
+  statistics <- mutate(statistics, mean = mean(value, na.rm = TRUE), .by = c("region", "period"))
+  df <- cbind.data.frame(df, 
+                         max = statistics[,"max"],  min = statistics[,"min"],  mean = statistics[,"mean"])
+  variable_names <- paste(unique(df[,"scenario"]), collapse = "\n")
   
-  for (ii in unique(x[["variable"]])) {
+  p <- ggplot(df, aes(period, value, colour = temp)) + 
+    geom_point()
     
-    df <- df[which(df[,"variable"] == ii), ]
-    model <- df["model"]
-    names(model) <- "r"
-    df[["model"]] <- paste0(df[["model"]], ".", df[["scenario"]])
-    df <- cbind(df, model)
-    fit95 <- mutate(df, max = max(value, na.rm = TRUE), .by = c("region", "period"))
-    fit95 <- mutate(fit95, min = min(value, na.rm = TRUE), .by = c("region", "period"))
-    fit95 <- mutate(fit95, mean = mean(value, na.rm = TRUE), .by = c("region", "period"))
-    df <- cbind.data.frame(df, 
-                           lwr95=fit95[,"max"],  upr95=fit95[,"min"],  upr=fit95[,"mean"])
-    names(df)[10] <- "max"
-    names(df)[11] <- "min"
-    names(df)[12] <- "mean"
-    variable_names <- paste(unique(df[,"scenario"]), collapse = "\n")
-    
-    p <- ggplot(df, aes(period, value, colour = r)) + 
-      geom_point()
-      
-      if (length(df) < 12) {
-        p <- p + ggtitle(paste0(df[,"region"]," ",df[,"variable"]))
-      } else {
-        p <- p + ggtitle(paste0(df[,"region"][1]," ", df[1,8]," ",df[,"variable"][1]))
-      }
-    
-    p <- p  +labs(x="period", y=df[,"unit"][1]) + 
-      theme_bw() + theme(legend.title=element_blank())
-    check <- (length(unique(df[,"r"])[unique(df[,"r"])>0 & (is.finite(unique(df[,"r"])))]))
-    check2 <- (length(unique(df[,"value"])[unique(df[,"value"])>0 & (is.finite(unique(df[,"value"])))]))
-    if ((check > 1) & (check2 > check)) {
-      p <- p + geom_line(aes(y = mean, color="mean"))
-      p <- p + geom_line(aes(y = max, color="max"))
-      p <- p + geom_line(aes(y = min, color="min"))
-      p <- p + geom_ribbon(aes(x = period, ymax = max, ymin = min), alpha = 0.6,
-                           fill = "grey70", color = "red", linetype = 0)
+    if (length(df) < 12) {
+      p <- p + ggtitle(paste0(df[,"region"]," ",df[,"variable"]))
+    } else {
+      p <- p + ggtitle(paste0(df[,"region"][1]," ", df[1,8]," ",df[,"variable"][1]))
     }
-    p <- p  + annotate("text", x = min(df[["period"]],na.rm = TRUE), y = max(df[["value"]],na.rm = TRUE), label = paste("Scenario:\n", variable_names), hjust = 0, vjust = 1, size = 3)
-    
-    ggsave(filename = paste0(df[,"region"][1]," ", df[1,8]," ",gsub("[|]", " ", ii),'.png'), plot = p,width = 7,height = 7)
-    
+  
+  p <- p  +labs(x="period", y=df[,"unit"][1]) + 
+    theme_bw() + theme(legend.title=element_blank())
+  check <- (length(unique(df[,"temp"])[unique(df[,"temp"])>0 & (is.finite(unique(df[,"temp"])))]))
+  check2 <- (length(unique(df[,"value"])[unique(df[,"value"])>0 & (is.finite(unique(df[,"value"])))]))
+  if ((check > 1) & (check2 > check)) {
+    p <- p + geom_line(aes(y = mean, color="mean"))
+    p <- p + geom_line(aes(y = max, color="max"))
+    p <- p + geom_line(aes(y = min, color="min"))
+    p <- p + geom_ribbon(aes(x = period, ymax = max, ymin = min), alpha = 0.6,
+                         fill = "grey70", color = "red", linetype = 0)
   }
+  p <- p  + annotate("text", x = min(df[["period"]],na.rm = TRUE), y = max(df[["value"]],na.rm = TRUE), label = paste("Scenario:\n", variable_names), hjust = 0, vjust = 1, size = 3)
+  
+  return(p)
 }
 
 
