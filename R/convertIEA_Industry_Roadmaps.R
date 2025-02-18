7611bbf5cc266ca9a78d70329853544ee354aaaf
sonja-sechi <sonja.sechi@ricardo.com>
2025-02-18 15:23:39 +0200

    convert function for IEA Industry Roadmap source added

--- R/convertIEA_Industry_Roadmaps.R
+++ R/convertIEA_Industry_Roadmaps.R
@@ -1,28 +1,19 @@
-#' convertIEA_Industry_Roadmaps
-#'
-#' 
-#' @param x MAgPIE object.
-#'
-#' @return blablabla
-#'
-#' @author Anastasis Giannousakis
-#'
-#' @examples
-#' \dontrun{
-#' a <- readSource("IEA_Industry_Roadmaps", convert = TRUE)
-#' }
-#'
-#'
 convertIEA_Industry_Roadmaps <- function(x) {
-
-#  map <- toolGetMapping("regionmappingH12.csv", where = "madrat")
-#  eu <- toolAggregate(x["EUR", , ], rel = map, partrel = TRUE)
   
-  getItems(x,1) <- toolCountry2isocode(getItems(x,1),
-                                               mapping = c("European Union" = "EUR",
-                                                           "Middle East" = "MEA"))
-  x <- toolCountryFill(x, fill = 0)
-
+  # Manually map of the regions, converting only China, India, and USA to ISO codes
+  # Assign custom codes for aggregated regions
+  x[["region"]] <- toolCountry2isocode(x[["region"]], mapping = c(
+    "China" = "CHN",
+    "India" = "IND",
+    "United States" = "USA",
+    "European Union" = "EUR",          
+    "Middle East" = "MEA",             
+    "Central and South America" = "CSA",
+    "Africa" = "AFR"
+  ))
+  
+  # Remove regions that are not in the mapping
+  x <- filter(x, !is.na(x[["region"]]))
+  
   return(x)
-
 }
