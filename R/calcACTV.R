a58036e47633e76c6b33d6fb4f7537d3af530aa6
Fsioutas <fsioutas@hotmail.com>
2025-04-22 16:32:28 +0300

    add units actv pop gdp

--- R/calcACTV.R
+++ R/calcACTV.R
@@ -120,7 +120,7 @@ calcACTV <- function() {
     select(-c("value.x", "value.y"))
   x <- as.quitte(qx) %>% as.magpie()
 
-  getNames(x) <- sub("\\..*$", "", getNames(x)) # remove units in the file read by GAMS
+  #getNames(x) <- sub("\\..*$", "", getNames(x)) # remove units in the file read by GAMS
 
   list(x = x,
        weight = NULL,
