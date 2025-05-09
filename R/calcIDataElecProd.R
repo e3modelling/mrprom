4afc95f2b2f21f29d50f537b725ca6ac02006d6b
Michael Madianos <mixalisspa@gmail.com>
2025-05-09 10:24:48 +0300

    Updated calcIDataElecProd.R to replace NA values with 0

--- R/calcIDataElecProd.R
+++ R/calcIDataElecProd.R
@@ -12,9 +12,8 @@
 #' }
 #'
 #' @importFrom dplyr filter %>% mutate select
-#' @importFrom tidyr pivot_wider
+#' @importFrom tidyr pivot_wider replace_na
 #' @importFrom quitte as.quitte
-
 
 calcIDataElecProd <- function() {
   share_of_solar <- calcOutput(type = "IInstCapPast", aggregate = FALSE)
@@ -108,38 +107,17 @@ calcIDataElecProd <- function() {
   # complete incomplete time series
   qx <- as.quitte(qx) %>%
     interpolate_missing_periods(period = getYears(x, as.integer = TRUE), expand.values = TRUE)
-  qx_bu <- qx
-  # assign to countries with NA, their H12 region mean
-  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
-  names(qx) <- sub("region", "CountryCode", names(qx))
-  ## add h12 mapping to dataset
-  qx <- left_join(qx, h12, by = "CountryCode")
-  ## add new column containing regional mean value
-  value <- NULL
-  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable"))
-  names(qx) <- sub("CountryCode", "region", names(qx))
-  qx <- select(qx, -c("model", "scenario", "X", "RegionCode"))
-  qx_bu <- select(qx_bu, -c("model", "scenario"))
-  ## assign to countries with NA, their H12 region mean
-  value.x <- NULL
-  value.y <- NULL
-  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
-    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
-    select(-c("value.x", "value.y"))
-  ## assign to countries that still have NA, the global mean
-  qx_bu <- qx
-  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("period", "variable"))
-  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
-    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
-    select(-c("value.x", "value.y"))
-  x <- as.quitte(qx) %>% as.magpie()
-  # set NA to 0
-  x[is.na(x)] <- 0
+
+  z <- qx %>%
+    replace_na(list("value" = 0)) %>%
+    select(c("region", "variable", "unit", "period", "value")) %>%
+    as.quitte() %>%
+    as.magpie()
 
   list(
-    x = collapseNames(x),
+    x = collapseNames(z),
     weight = NULL,
-    unit = getItems(x, 3.2)[1],
+    unit = getItems(z, 3.2)[1],
     description = "Enerdata; Electricity production"
   )
 }
