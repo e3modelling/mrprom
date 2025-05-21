375b9ec513ee5626f72a99c2922c38d8bcd9623f
Michael Madianos <mixalisspa@gmail.com>
2025-05-21 10:21:27 +0300

    Updated calcIInstCapPast.R & calcIDataElecSteamGen.R to fill NAs with 0s

--- R/calcIDataElecSteamGen.R
+++ R/calcIDataElecSteamGen.R
@@ -16,118 +16,15 @@
 #' @importFrom quitte as.quitte
 
 calcIDataElecSteamGen <- function() {
-  
-  x <- readSource("ENERDATA", "capacity", convert = TRUE)
-  # filter years
-  fStartHorizon <- readEvalGlobal(system.file(file.path("extdata", "main.gms"), package = "mrprom"))["fStartHorizon"]
-  x <- x[, c(max(fStartHorizon, min(getYears(x, as.integer = TRUE))) : max(getYears(x, as.integer = TRUE))), ]
-  
-  # use enerdata-openprom mapping to extract correct data from source
-  map <- toolGetMapping(name = "prom-enerdata-pgall-mapping.csv",
-                        type = "sectoral",
-                        where = "mrprom")
-  ## ..and only items that have an enerdata-prom mapping
-  enernames <- unique(map[!is.na(map[, "ENERDATA..MW."]), "ENERDATA..MW."])
-  map <- map[map[, "ENERDATA..MW."] %in% enernames, ]
-  
-  enernames <- unique(map[!is.na(map[, "ENERDATA..MW."]), "ENERDATA..MW."])
-  enernames <- enernames[! enernames %in% c("")]
-  
-  z <- enernames == "Total electricity capacity coal, lignite (multifuel included) - Single fired electricity capacity lignite"
-  enernames[z] <- "Total electricity capacity coal, lignite (multifuel included)"
-  k <- enernames == "Total electricity capacity gas (multifuel oil/gas included) - Installed electricity capacity of co-generation gas"
-  enernames[k] <- "Total electricity capacity gas (multifuel oil/gas included)"
-  p <- enernames == "(Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%)*(Total electricity capacity coal, lignite (multifuel included) - Single fired electricity capacity lignite)"
-  enernames[p] <- "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"
-  
-  x <- x[, , enernames]
-  
-  x2 <- x[, , "Single fired electricity capacity lignite"]
-  x4 <- x[, , "Installed electricity capacity of co-generation gas"]
-  d <- x[, , "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"] / 100
-  
-  x[, , "Total electricity capacity coal, lignite (multifuel included)"] <- x[, , "Total electricity capacity coal, lignite (multifuel included)"] - ifelse(is.na(x2), 0, x2)
-  x[, , "Total electricity capacity gas (multifuel oil/gas included)"] <- x[, , "Total electricity capacity gas (multifuel oil/gas included)"] - ifelse(is.na(x4), 0, x4)
-  x[, , "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"] <- x[, , "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"] * x[, , "Total electricity capacity coal, lignite (multifuel included)"] / 100
-  
-  # remove from coal the Supercritical coal
-  x[, , "Total electricity capacity coal, lignite (multifuel included)"] <- x[, , "Total electricity capacity coal, lignite (multifuel included)"] * (1 - ifelse(is.na(d), 0, d))
-  
-  l <- getNames(x) == "Total electricity capacity coal, lignite (multifuel included).MW"
-  getNames(x)[l] <- "Total electricity capacity coal, lignite (multifuel included).MW - Single fired electricity capacity lignite.MW"
-  v <- getNames(x) == "Total electricity capacity gas (multifuel oil/gas included).MW"
-  getNames(x)[v] <- "Total electricity capacity gas (multifuel oil/gas included).MW - Installed electricity capacity of co-generation gas.MW"
-  m <- getNames(x) == "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"
-  getNames(x)[m] <- "(Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%)*(Total electricity capacity coal, lignite (multifuel included) - Single fired electricity capacity lignite)"
-  
-  ## rename variables from ENERDATA to openprom names
-  ff <- map[!(map[, 2] == ""), 1]
-  getNames(x) <- ff
-  
-  qx <- as.quitte(x) %>%
-    interpolate_missing_periods(period = getYears(x, as.integer = TRUE), expand.values = TRUE)
-  qx_bu <- qx
-  # assign to countries with NA, their H12 region mean
-  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
-  names(qx) <- sub("region", "CountryCode", names(qx))
-  ## add h12 mapping to dataset
-  qx <- left_join(qx, h12, by = "CountryCode")
-  ## add new column containing regional mean value
-  value <- NULL
-  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable"))
-  names(qx) <- sub("CountryCode", "region", names(qx))
-  qx <- select(qx, -c("model", "scenario", "X", "RegionCode"))
-  qx_bu <- select(qx_bu, -c("model", "scenario"))
-  ## assign to countries with NA, their H12 region mean
-  value.x <- NULL
-  value.y <- NULL
-  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
-    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
-    select(-c("value.x", "value.y"))
-  ## assign to countries that still have NA, the global mean
-  qx_bu <- qx
-  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("period", "variable"))
-  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
-    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
-    select(-c("value.x", "value.y"))
-  
-  # Multiplying each power plant tech by availability rate (from PRIMES)
-  variable <- NULL
-  TOTCAP <- NULL
-  PEAKLOAD <- NULL
-  region <- NULL
-  period <- NULL
-  unit <- NULL
-  
-  qx <- qx %>%
-    mutate(value = case_when(
-      variable == "ATHLGN" ~ value * 0.85,
-      variable == "ATHHCL" ~ value * 0.85,
-      variable == "SUPCR" ~ value * 0.85,
-      variable == "ATHRFO" ~ value * 0.80,
-      variable == "ATHNGS" ~ value * 0.80,
-      variable == "ATHBMSWAS" ~ value * 0.85,
-      variable == "ACCGT" ~ value * 0.80,
-      variable == "PGANUC" ~ value * 0.90,
-      variable == "PGLHYD" ~ value * 0.67,
-      variable == "PGWND" ~ value * 0.23,
-      variable == "PGSOL" ~ value * 0.20,
-      variable == "PGASOL" ~ value * 0.25,
-      variable == "PGAWNO" ~ value * 0.32,
-      variable == "PGAOTHREN" ~ value * 0.45,
-      TRUE ~ value))
-  
-  # Calculating TOTCAP as a sum of all capacities
-  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("region", "period"))
-  qx["variable"] <- "TOTCAP"
-  qx <- unique(qx)
-  # Convert TOTCAP unit to GW
-  qx <- qx %>%
-    mutate(value = if_else(variable == "TOTCAP", value / 1000, value))
-  
-  # Adding PEAKLOAD and other variables
-  qx <- qx %>%
-    spread(key = variable, value = value) %>%
+  EffCapacities <- calcOutput(type = "IInstCapPast", aggregate = FALSE)
+
+  ElecSteamGen <- EffCapacities %>%
+    as.quitte() %>%
+    mutate(TOTCAP = sum(value, na.rm = TRUE), .by = c("region", "period")) %>%
+    select(c("region", "period", "TOTCAP")) %>%
+    pivot_longer(cols = "TOTCAP", names_to = "variable", values_to = "value") %>%
+    unique() %>%
+    pivot_wider(names_from = "variable", values_from = "value") %>%
     mutate(
       PEAKLOAD = TOTCAP * 0.9,
       BASELOAD = PEAKLOAD * 0.3576,
@@ -138,16 +35,16 @@ calcIDataElecSteamGen <- function() {
       # STE1CR = 0.0, STE1CG = 0.0, STE1CB = 0.0,
       STE1AL = 0.0, STE1AH = 0.0, STE1AD = 0.0,
       STE1AR = 0.0, STE1AG = 0.0, STE1AB = 0.0,
-      STE1AH2F = 0.0) %>%
-    gather(key = "variable", value = "value", -region, -period, -unit) %>%
-    arrange(region, period, variable)
-  
-  # Converting to magpie object
-  x <- as.quitte(qx) %>% as.magpie()
-  # set NA to 0
-  x[is.na(x)] <- 0
-  list(x = x,
-       weight = NULL,
-       unit = "GW",
-       description = "Enerdata; Installed capacity")
+      STE1AH2F = 0.0
+    ) %>%
+    pivot_longer(cols = -c("region", "period"), names_to = "variable", values_to = "value") %>%
+    as.quitte() %>%
+    as.magpie()
+
+  list(
+    x = ElecSteamGen,
+    weight = NULL,
+    unit = "GW",
+    description = "Enerdata; Installed capacity"
+  )
 }
--- R/calcIInstCapPast.R
+++ R/calcIInstCapPast.R
@@ -13,64 +13,67 @@
 #' }
 #'
 #' @importFrom dplyr %>% select mutate left_join case_when if_else arrange
-#' @importFrom tidyr pivot_wider spread gather
+#' @importFrom tidyr pivot_wider spread gather replace_na
 #' @importFrom quitte as.quitte
 #' @importFrom tibble add_row
 
 calcIInstCapPast <- function() {
-  
   x <- readSource("ENERDATA", "capacity", convert = TRUE)
-  avail <- calcOutput(type = "IAvailRate", aggregate = FALSE)
-  
+
   # filter years
   fStartHorizon <- readEvalGlobal(system.file(file.path("extdata", "main.gms"), package = "mrprom"))["fStartHorizon"]
-  x <- x[, c(max(fStartHorizon, min(getYears(x, as.integer = TRUE))) : max(getYears(x, as.integer = TRUE))), ]
-  
+  years <- getYears(x, as.integer = TRUE)
+  x <- x[, c(max(fStartHorizon, min(years)):max(years)), ]
+
   # use enerdata-openprom mapping to extract correct data from source
-  map <- toolGetMapping(name = "prom-enerdata-pgall-mapping.csv",
-                        type = "sectoral",
-                        where = "mrprom")
+  map <- toolGetMapping(
+    name = "prom-enerdata-pgall-mapping.csv",
+    type = "sectoral",
+    where = "mrprom"
+  )
   ## ..and only items that have an enerdata-prom mapping
   enernames <- unique(map[!is.na(map[, "ENERDATA..MW."]), "ENERDATA..MW."])
   map <- map[map[, "ENERDATA..MW."] %in% enernames, ]
-  
+
   enernames <- unique(map[!is.na(map[, "ENERDATA..MW."]), "ENERDATA..MW."])
-  enernames <- enernames[! enernames %in% c("")]
-  
+  enernames <- enernames[!enernames %in% c("")]
+
   z <- enernames == "Total electricity capacity coal, lignite (multifuel included) - Single fired electricity capacity lignite"
   enernames[z] <- "Total electricity capacity coal, lignite (multifuel included)"
   k <- enernames == "Total electricity capacity gas (multifuel oil/gas included) - Installed electricity capacity of co-generation gas"
   enernames[k] <- "Total electricity capacity gas (multifuel oil/gas included)"
   p <- enernames == "(Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%)*(Total electricity capacity coal, lignite (multifuel included) - Single fired electricity capacity lignite)"
   enernames[p] <- "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"
-  
+
   x <- x[, , enernames]
-  
+
   b <- x[, , "Single fired electricity capacity lignite"]
   c <- x[, , "Installed electricity capacity of co-generation gas"]
   d <- x[, , "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"] / 100
-  
+
   x[, , "Total electricity capacity coal, lignite (multifuel included)"] <- x[, , "Total electricity capacity coal, lignite (multifuel included)"] - ifelse(is.na(b), 0, b)
   x[, , "Total electricity capacity gas (multifuel oil/gas included)"] <- x[, , "Total electricity capacity gas (multifuel oil/gas included)"] - ifelse(is.na(c), 0, c)
   x[, , "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"] <- x[, , "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"] * x[, , "Total electricity capacity coal, lignite (multifuel included)"] / 100
-  
+
   # remove from coal the Supercritical coal
   x[, , "Total electricity capacity coal, lignite (multifuel included)"] <- x[, , "Total electricity capacity coal, lignite (multifuel included)"] * (1 - ifelse(is.na(d), 0, d))
-  
-  
+
+
   l <- getNames(x) == "Total electricity capacity coal, lignite (multifuel included).MW"
   getNames(x)[l] <- "Total electricity capacity coal, lignite (multifuel included).MW - Single fired electricity capacity lignite.MW"
   v <- getNames(x) == "Total electricity capacity gas (multifuel oil/gas included).MW"
   getNames(x)[v] <- "Total electricity capacity gas (multifuel oil/gas included).MW - Installed electricity capacity of co-generation gas.MW"
   m <- getNames(x) == "Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%"
   getNames(x)[m] <- "(Share of supercritical, ultrasupercritical and IGCC technologies in coal installed capacity.%)*(Total electricity capacity coal, lignite (multifuel included) - Single fired electricity capacity lignite)"
-  
+
   ## rename variables from ENERDATA to openprom names
-  ff <- map[!(map[, 2] == ""), 1]
-  getNames(x) <- ff
-  
+  getNames(x) <- map[!(map[, 2] == ""), 1]
+
   qx <- as.quitte(x) %>%
+    replace_na(list("value" = 0)) %>%
     interpolate_missing_periods(period = getYears(x, as.integer = TRUE), expand.values = TRUE)
+
+  ' # legacy
   qx_bu <- qx
   # assign to countries with NA, their H12 region mean
   h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
@@ -95,24 +98,23 @@ calcIInstCapPast <- function() {
   qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
     mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
     select(-c("value.x", "value.y"))
-  
+'
   # Multiplying the capacity values by the availability rate
-  avail_rates <- as.quitte(avail["GLO", "y2020",])[c("variable", "value")]
-  
-  qx <- qx %>%
+  avail <- calcOutput(type = "IAvailRate", aggregate = FALSE)
+  avail_rates <- as.quitte(avail["GLO", "y2020", ])[c("variable", "value")]
+
+  EffCapacities <- qx %>%
     left_join(avail_rates, by = "variable") %>%
-    mutate(value = value.x * value.y) %>%
-    select(-c("value.x", "value.y"))
-  
-  # Converting MW values to GW
-  qx[["value"]] <- qx[["value"]] / 1000
-  
-  # Converting to magpie object
-  x <- as.quitte(qx) %>% as.magpie()
-  # set NA to 0
-  x[is.na(x)] <- 0
-  list(x = x,
-       weight = NULL,
-       unit = "GW",
-       description = "Enerdata; Installed capacity")
+    # Applying avail rates & converting MW values to GW
+    mutate(value = value.x * value.y / 1000) %>%
+    select(c("region", "variable", "period", "value")) %>%
+    as.quitte() %>%
+    as.magpie()
+
+  list(
+    x = EffCapacities,
+    weight = NULL,
+    unit = "GW",
+    description = "Enerdata; Installed capacity"
+  )
 }
