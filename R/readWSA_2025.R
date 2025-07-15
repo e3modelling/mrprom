2497005da8f1c5a63a9946aa8345d07bfd25446b
Fsioutas <fsioutas@hotmail.com>
2025-07-15 15:38:04 +0300

    add readWSA_2025

--- R/readWSA_2025.R
+++ R/readWSA_2025.R
@@ -2,13 +2,13 @@
 #'
 #' Read World Steel Association Crude Steel production by process 2024
 #'
-#' @return The read-in data into a magpie object.
+#' @return The read-in data into a quitte object.
 #'
 #' @author Fotis Sioutas
 #'
 #' @examples
 #' \dontrun{
-#' a <- readSource("WSA_2025", convert = TRUE)
+#' a <- readSource("WSA_2025")
 #' }
 #'
 #' @importFrom readxl read_excel
@@ -18,47 +18,42 @@
 #'
 readWSA_2025 <- function() {
   
-  x <- read_excel("Steel_Production_USGS_myb1-2023-feste-ert.xlsx",
-                  sheet = "T10")
+  x <- read_excel("ISProd_by_process_2024.xlsx")
   
-  names(x) <- x[5,]
-  x <- x[6:96,c(1,3,5,7,9,11)]
+  names(x) <- x[1,]
   
-  x <- x %>%
-    mutate(across(-`Country or locality3`, as.character))
+  x <- x[3:51,]
   
-  x <- x %>% pivot_longer(!("Country or locality3"), names_to = "period", values_to = "value")
+  names(x) <- sub("Country or region","region",names(x))
+  names(x) <- sub("Year","period",names(x))
+  names(x) <- sub("IS_Prod_WSA","value",names(x))
   
-  names(x)[1] <- "region"
   x["value"] <- as.numeric(x[["value"]])
+  x["BOFshare"] <- as.numeric(x[["BOFshare"]])
+  x["EAFshare"] <- as.numeric(x[["EAFshare"]])
+  x["OtherISROUTEshare"] <- as.numeric(x[["OtherISROUTEshare"]])
   x <- as.quitte(x)
+  x["unit"] <- "Mt"
+  x["variable"] <- "Crude Steel production"
   x <- filter(x, !is.na(x[["value"]]))
   x <- filter(x, !is.na(x[["region"]]))
-  x["unit"] <- "Mt"
-  x["variable"] <- "IS"
   
   x[["region"]] <- toolCountry2isocode((x[["region"]]), mapping =
-                                         c("Bangladeshe" = "BGD",
-                                           "Burmae" = "MMR",
-                                           "Jordane" = "JOR",
-                                           "Kenyae" = "KEN",
-                                           "Korea, North" = "PRK",
-                                           "Montenegroe" = "MNE",
-                                           "Nigeriae" = "NGA",
-                                           "Omane" = "OMN",
-                                           "Rwandae" = "RWA",
-                                           "Tanzaniae" = "TZA",
-                                           "Zambiae" = "ZMB",
-                                           "Mauritaniae" = "MRT"))
+                                         c("Other CIS" = "Other CIS",
+                                           "Other Middle East" = "Other Middle East",
+                                           "Other North America" = "Other North America",
+                                           "Other South America" = "Other South America",
+                                           "Others" = "Others",
+                                           "Taiwan, China" = "TWN"))
   x <- as.quitte(x)
   x <- filter(x, !is.na(x[["region"]]))
-  x <- as.magpie(x)
   
   list(x = x,
        weight = NULL,
+       class = "quitte",
        description = c(category = "World Steel Association Crude Steel production by process 2024",
                        type = "World Steel Association Crude Steel production by process 2024",
-                       filename = "ISProd_by process_2024.xlsx",
+                       filename = "ISProd_by_process_2024.xlsx",
                        `Indicative size (MB)` = 0.37,
                        dimensions = "2D",
                        unit = "Mt",
