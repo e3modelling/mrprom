1d93a3812b72ab6cf14445c8c075a53c41313a9d
Michael Madianos <mixalisspa@gmail.com>
2025-09-04 18:12:58 +0300

    Added new calc functions for Transport SFCs & historical car stock per tech

---
+++ R/calcISFC.R
@@ -0,0 +1,187 @@
+#' calcISFC
+#'
+#' Derive Specific fuel consumption per car technology for all countries
+#'
+#' @return magpie object
+#'
+#' @author Michael Madianos, Anastasis Giannousakis
+#'
+#' @examples
+#' \dontrun{
+#' SFC <- calcOutput("ISFC", aggregate = FALSE)
+#' }
+#'
+#' @importFrom dplyr filter %>%  mutate rename select group_by summarise ungroup inner_join left_join rowwise bind_rows reframe
+#' @importFrom tidyr replace_na
+#' @importFrom magclass as.magpie
+#' @importFrom quitte as.quitte
+#' @importFrom tibble deframe
+calcISFC <- function() {
+  mappingTechnologies <- data.frame(
+    category = c(
+      "Internal combustion engine", "Internal combustion engine",
+      "Internal combustion engine", "Internal combustion engine",
+      "Internal combustion engine", "Internal combustion engine",
+      "Conv_ Hybrid", "Conv_ Hybrid",
+      "Plug In Hybrid", "Plug In Hybrid", "Plug In Hybrid",
+      "Pure Electric Vehicles", "Fuel cells and other"
+    ),
+    fuel = c(
+      "LPG", "Gasoline", "Diesel oil", "Diesel oil",
+      "Natural gas", "Ethanol", "Gasoline", "Diesel oil",
+      "Gasoline", "Diesel oil", "Electricity",
+      "Pure Electric Vehicles", "Hydrogen"
+    ),
+    code = c(
+      "LPG", "GSL", "GDO", "BGDO", "NGS",
+      "ETH", "CHEVGSL", "CHEVGDO",
+      "PHEVGSL", "PHEVGDO", "PHEVELC",
+      "ELC", "H2F"
+    ),
+    stringsAsFactors = FALSE
+  )
+  mappingTechnologiesToFuels <- mappingTechnologies %>%
+    select(code) %>%
+    mutate(
+      fuel = code,
+      fuel = case_when(
+        code == "PHEVGSL" ~ "GSL",
+        code == "PHEVGDO" ~ "GDO",
+        code == "CHEVGSL" ~ "GSL",
+        code == "CHEVGDO" ~ "GDO",
+        TRUE ~ code
+      )
+    ) %>%
+    deframe()
+
+  SFC <- helpGetHistoricalSFC(mappingTechnologies)
+  SFC <- helperCorrectSFC(SFC) %>%
+    mutate(fuel = recode(tech, !!!mappingTechnologiesToFuels))
+
+  # Transfer PHEVELC from technologies to fuel mode of all plug-ins
+  tempPHEVELC <- SFC %>%
+    filter(tech == "PHEVELC") %>%
+    rowwise() %>%
+    reframe(
+      region = region,
+      period = period,
+      value = value,
+      tech = c("PHEVGSL", "PHEVGDO"),
+      fuel = "ELC"
+    )
+
+  SFC <- SFC %>%
+    filter(tech != "PHEVELC") %>%
+    bind_rows(tempPHEVELC) %>%
+    as.quitte() %>%
+    as.magpie()
+
+  list(
+    x = SFC,
+    weight = NULL,
+    unit = "toe per vkm",
+    description = "Primes; Specific fuel consumption"
+  )
+}
+
+helpGetHistoricalSFC <- function(mappingTechnologies) {
+  stockPC <- calcOutput(type = "ACTV", aggregate = FALSE) %>%
+    as.quitte() %>%
+    filter(variable == "PC", !is.na(value), value != 0) %>%
+    rename(stock = value)
+
+  # European SFCs from Primes
+  SFCEU <- readSource("PrimesNewTransport", subtype = "Indicators") %>%
+    as.quitte() %>%
+    interpolate_missing_periods(period = 2015:2100, expand.values = TRUE) %>%
+    filter(period >= 2010, period <= 2020, sector == "PC") %>%
+    inner_join(mappingTechnologies, by = c("category", "fuel"), relationship = "many-to-many") %>%
+    rename(tech = code) %>%
+    select(c("region", "period", "unit", "tech", "value")) %>%
+    mutate(unit = "ktoe/Gvkm")
+
+  regionsEU <- unique(SFCEU$region)
+
+  # Average SFC for EU countries
+  meanSFCEU <- SFCEU %>%
+    group_by(period, tech) %>%
+    summarise(meanSFC = mean(value, na.rm = TRUE), .groups = "drop")
+
+  # Calculate total Final energy per country and scale it with car stock and mean km
+  estimateSFCGlobal <- calcOutput(type = "IFuelCons", subtype = "TRANSE", aggregate = FALSE) %>%
+    as.quitte() %>%
+    rename(tech = new) %>%
+    filter(variable == "PC") %>%
+    group_by(region, period) %>%
+    summarise(value = sum(value, na.rm = TRUE), .groups = "drop") %>%
+    left_join(stockPC, by = c("region", "period")) %>%
+    mutate(value = value / stock / 15000) %>%
+    select(region, period, value)
+
+  # Estimated general SFC for EU countries
+  estimateSFCEU <- estimateSFCGlobal %>%
+    filter(region %in% regionsEU) %>%
+    group_by(period) %>%
+    summarise(meanSFCEU = mean(value, na.rm = TRUE))
+
+  # Estimate SFC per tech by scaling the european SFC by the ratio of aggregate SFCs
+  SFC <- estimateSFCGlobal %>%
+    left_join(meanSFCEU, by = "period", relationship = "many-to-many") %>%
+    left_join(estimateSFCEU, by = c("period")) %>%
+    mutate(value = meanSFC * value / meanSFCEU) %>%
+    left_join(SFCEU, by = c("region", "period", "tech")) %>%
+    mutate(
+      value = ifelse(!is.na(value.y), value.y, value.x),
+      unit = "toe per vkm"
+    ) %>%
+    select(region, period, tech, unit, value)
+  return(SFC)
+}
+
+helperCorrectSFC <- function(SFC) {
+  baselineCountry <- "AUT"
+
+  correctedSFC <- filter(SFC, !is.na(value))
+  baselineSFC <- correctedSFC %>%
+    filter(region == baselineCountry) %>%
+    select(-c("region", "unit")) %>%
+    rename(base = value)
+
+  # keep entries that are less than 50% divergent from baseline country
+  # For cars of new technologies, keep baseline SFC
+  newTechs <- c(
+    "ETH", "CHEVGSL", "CHEVGDO",
+    "PHEVGSL", "PHEVGDO", "PHEVELC",
+    "ELC", "H2F"
+  )
+  correctedSFC <- correctedSFC %>%
+    left_join(baselineSFC, by = c("period", "tech")) %>%
+    filter(abs(value / base - 1) < 0.9) %>%
+    mutate(value = ifelse(tech %in% newTechs, base, value)) %>%
+    filter(period == 2020) %>%
+    select(-base) %>%
+    as.quitte() %>%
+    as.magpie()
+
+  # Now apply toolCountryFill if needed for other types of gaps
+  temp <- toolCountryFill(correctedSFC) %>%
+    as.quitte()
+
+  # Fill NAs with baseline country
+  correctedSFC <- temp %>%
+    left_join(
+      filter(temp, region == baselineCountry) %>% select(-region),
+      by = c("period", "tech")
+    ) %>%
+    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
+    select(region, period, tech, value)
+}
+
+helperEstimateOtherPassModes <- function() {
+  df <- data.frame(
+    mode = c("PT", "PT", "PT", "PT", "PA", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GU", "GT", "GT", "GT", "GT", "GN", "GN", "GN"),
+    tech = c("GDO", "MET", "H2F", "ELC", "H2F", "LPG", "GSL", "GDO", "NGS", "MET", "ETH", "BGDO", "H2F", "ELC", "PHEVGSL", "PHEVGSL", "PHEVGDO", "PHEVGDO", "CHEVGDO", "GDO", "MET", "H2F", "ELC", "GSL", "GDO", "H2F"),
+    fuel = c("GDO", "MET", "H2F", "ELC", "H2F", "LPG", "GSL", "GDO", "NGS", "MET", "ETH", "BGDO", "H2F", "ELC", "GSL", "ELC", "GDO", "ELC", "GDO", "GDO", "MET", "H2F", "ELC", "GSL", "GDO", "H2F"),
+    value = c(18.6313, 12.6, 8.9, 2.73638, 21.7, 54.1073, 60.1192, 45.0894, 66, 56.2, 80, 45.0894, 13.5268, 27.0536, 34.4, 21.8, 27.0536, 21.8, 21.8, 33.629, 78, 92, 11.5245, 22.8, 15.2, 8.14286)
+  )
+}
---
+++ R/calcStockPC.R
@@ -0,0 +1,184 @@
+#' calcStockPC
+#'
+#' Derive car stock per fuel technology for all countries.
+#'
+#' @return magpie object
+#'
+#' @author Michael Madianos, Anastasis Giannousakis
+#'
+#' @examples
+#' \dontrun{
+#' stockPC <- calcOutput("StockPC", aggregate = FALSE)
+#' }
+#'
+#' @importFrom dplyr filter %>% mutate select rename group_by summarise ungroup inner_join full_join right_join recode
+#' @importFrom tidyr expand_grid replace_na crossing
+#' @importFrom magclass as.magpie
+#' @importFrom quitte as.quitte
+#'
+calcStockPC <- function() {
+  mappingEVs <- list(
+    "BEV" = "ELC",
+    "PHEVGDO" = "PHEVGDO",
+    "PHEVGSL" = "PHEVGSL",
+    "FCEV" = "H2F"
+  )
+
+  technologyMapping <- list(
+    "CNG" = "NGS",
+    "Diesel Conventional" = "GDO",
+    "Diesel Hybrid" = "CHEVGDO",
+    "Diesel plug-in hybrid" = "PHEVGDO",
+    "E85" = "MET",
+    "Electric" = "ELC",
+    "Gasoline Conventional" = "GSL",
+    "Gasoline Hybrid" = "CHEVGSL",
+    "Gasoline plug-in hybrid" = "PHEVGSL",
+    "Hydrogen" = "H2F",
+    "LPG" = "LPG"
+  )
+
+  stockEU <- readSource("PrimesNewTransport", subtype = "Stock") %>%
+    as.quitte() %>%
+    filter(period >= 2010, sector == "PC") %>%
+    mutate(
+      variable = recode(variable, !!!technologyMapping),
+      value = value / 1e3,
+      unit = "million vehicles"
+    )
+
+  SFC <- calcOutput(type = "ISFC", aggregate = FALSE) %>%
+    as.quitte() %>%
+    select(-fuel) %>%
+    rename(SFC = value)
+
+  carStockTotal <- calcOutput(type = "ACTV", file = "iACTV.csv", aggregate = FALSE) %>%
+    as.quitte() %>%
+    filter(variable == "PC", period >= 2015) %>%
+    rename(stock = value)
+
+  shareEVs <- helperGetEVShares(mappingEVs)
+  shareNonEVs <- helperGetNonEVShares(SFC, mappingEVs)
+
+  stockEV <- carStockTotal %>%
+    inner_join(shareEVs, by = c("region", "period")) %>%
+    mutate(
+      stock = stock * share
+    ) %>%
+    select(region, period, tech, stock)
+
+  stockTotalEV <- stockEV %>%
+    group_by(region, period) %>%
+    summarise(value = sum(stock), .groups = "drop")
+
+  stockNonEV <- carStockTotal %>%
+    inner_join(stockTotalEV, by = c("region", "period")) %>%
+    mutate(stock = stock - value) %>%
+    right_join(shareNonEVs, by = c("region", "period")) %>%
+    mutate(
+      stock = stock * share
+    ) %>%
+    select(region, period, tech, stock)
+
+  stock <- stockNonEV %>%
+    full_join(stockEV, by = c("region", "period", "tech")) %>%
+    filter(period == 2020) %>%
+    mutate(stock = ifelse(is.na(stock.x), stock.y, stock.x)) %>%
+    select(region, period, tech, stock) %>%
+    rename(value = stock) %>%
+    as.quitte() %>%
+    as.magpie()
+
+  list(
+    x = stock,
+    weight = NULL,
+    unit = "million vehicles",
+    description = "Activity data for OPENPROM sectors"
+  )
+}
+# -------------------------------------------------------------------
+helperGetEVShares <- function(mappingEVs) {
+  sharesEVTechs <- readSource("IEA_EV", convert = TRUE) %>%
+    as.quitte() %>%
+    filter(
+      parameter == "EV stock",
+      category == "Historical",
+      variable == "Cars",
+      !is.na(value),
+      period <= 2020
+    ) %>%
+    # Split PHEV into PHEVGSL and PHEVGDO
+    mutate(
+      value = if_else(powertrain == "PHEV", value * 0.5, value),
+      powertrain = case_when(
+        powertrain == "PHEV" ~ "PHEVGSL",
+        TRUE ~ powertrain
+      )
+    )
+
+  phevgdo <- sharesEVTechs %>%
+    filter(powertrain == "PHEVGSL") %>%
+    mutate(
+      powertrain = case_when(
+        powertrain == "PHEVGSL" ~ "PHEVGDO",
+        TRUE ~ powertrain
+      )
+    )
+
+  # Add PHEVGDO and calculate shares between EVs
+  sharesEVTechs <- sharesEVTechs %>%
+    bind_rows(phevgdo) %>%
+    # calculate relative % of EVs
+    group_by(region, period) %>%
+    mutate(
+      total_value = sum(value),
+      share = value / total_value
+    ) %>%
+    ungroup() %>%
+    select(region, variable, period, share, powertrain) %>%
+    mutate(powertrain = recode(powertrain, !!!mappingEVs))
+
+  stockSharesEV <- readSource("IEA_EV", convert = TRUE) %>%
+    as.quitte() %>%
+    filter(
+      parameter == "EV stock share",
+      category == "Historical",
+      variable == "Cars",
+      !is.na(value),
+      period <= 2020
+    ) %>%
+    right_join(sharesEVTechs, by = c("region", "period")) %>%
+    mutate(share = value * share / 100) %>%
+    select(region, period, powertrain.y, share) %>%
+    rename(tech = powertrain.y)
+
+  # Fill rest of countries with share 0
+  stockSharesEV <- expand_grid(
+    region = unname(getISOlist()),
+    period = unique(stockSharesEV$period),
+    tech = unique(stockSharesEV$tech)
+  ) %>%
+    left_join(stockSharesEV, by = c("region", "period", "tech")) %>%
+    mutate(share = replace_na(share, 0))
+}
+
+helperGetNonEVShares <- function(SFC, mappingEVs) {
+  shareNonEVs <- calcOutput(type = "IFuelCons", subtype = "TRANSE", aggregate = FALSE) %>%
+    as.quitte() %>%
+    rename(tech = new) %>%
+    filter(variable == "PC") %>%
+    right_join(SFC, by = c("region", "period", "tech")) %>%
+    mutate(value = replace_na(value, 0) / SFC) %>%
+    filter(
+      !is.na(value),
+      !tech %in% unlist(mappingEVs, use.names = F)
+    ) %>%
+    # Calculate relative % of techs
+    group_by(region, period) %>%
+    mutate(
+      total_value = sum(value),
+      share = value / total_value
+    ) %>%
+    ungroup() %>%
+    select(region, period, tech, share)
+}
