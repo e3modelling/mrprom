f793198b8f3d18ec2078ac26c890cd66c61eb2d8
Fsioutas <fsioutas@hotmail.com>
2025-10-01 17:35:27 +0300

    fix_emi_from_nav

--- R/calcNavigateEmissions.R
+++ R/calcNavigateEmissions.R
@@ -46,6 +46,63 @@ calcNavigateEmissions <- function() {
   
   x <- as.quitte(x)
   x <- interpolate_missing_periods(x, 2010:2100, expand.values = TRUE)
+  
+  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
+  
+  qx <- select(x, -c("model", "scenario"))
+  qx_bu <- qx
+  
+  ## assign to countries with NA, their H12 region with weights calculated from population
+  
+  population <- calcOutput(type = "POP", aggregate = FALSE)
+  population <- as.quitte(population)
+  
+  # compute weights by population
+  names(population) <- sub("region", "CountryCode", names(population))
+  
+  ## add mapping to population
+  population <- left_join(population, h12, by = "CountryCode")
+  value.x <- NULL
+  value.y <- NULL
+  weights <- NULL
+  value <- NULL
+  POP <- mutate(population, weights = sum(value, na.rm = TRUE), .by = c("RegionCode", "period"))
+  POP["weights"] <- POP["value"] / POP["weights"]
+  
+  names(POP) <- sub("CountryCode", "region", names(POP))
+  POP <- select(POP, -c("value", "model", "scenario", "X", "variable", "unit"))
+  qx <- left_join(qx, POP, by = c("region", "period"))
+  
+  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable", "unit"))
+  
+  qx["value"] <- qx["value"] * qx["weights"]
+  
+  qx <- select(qx, -c("weights"))
+  
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+    mutate(value = ifelse(is.na(value.x) & value.x != 0, value.y, value.x)) %>%
+    select(-c("value.x", "value.y", "RegionCode"))
+  
+  ## assign to countries that still have NA, the global with weights
+  qx_bu <- qx
+  # compute weights by population
+  POP <- mutate(population, weights = sum(value, na.rm = TRUE), .by = c("period"))
+  POP["weights"] <- POP["value"] / POP["weights"]
+  names(POP) <- sub("CountryCode", "region", names(POP))
+  POP <- select(POP, -c("value", "model", "scenario", "X", "RegionCode", "variable", "unit"))
+  qx <- left_join(qx, POP, by = c("region", "period"))
+  
+  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("period", "variable", "unit"))
+  
+  qx["value"] <- qx["value"] * qx["weights"]
+  
+  qx <- select(qx, -c("weights"))
+  
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
+    select(-c("value.x", "value.y"))
+  x <- as.quitte(qx) %>% as.magpie()
+  
   x <- as.magpie(x)
   
   # set NA to 0
