3b96d4c91fdcf4cafc92651591429acc9ef7c7c5
Fsioutas <fsioutas@hotmail.com>
2025-10-02 11:10:14 +0300

    select less emissions

--- R/calcNavigateEmissions.R
+++ R/calcNavigateEmissions.R
@@ -28,6 +28,15 @@ calcNavigateEmissions <- function() {
                         type = "sectoral",
                         where = "mrprom")
   
+  map <- map[map[,1] %in% c("Emissions|CO2|AFOLU","Emissions|BC","Emissions|CH4",
+                            "Emissions|NH3","Emissions|NOx","Emissions|OC",
+                            "Emissions|SF6","Emissions|Sulfur","Emissions|VOC",
+                            "Emissions|CO2|Industrial Processes"), 1]
+  
+  map <- as.data.frame(map)
+  
+  names(map) <- "Emissions"
+  
   #filter navigate data by variable
   x1 <- readSource("Navigate", subtype = "SUP_NPi_Default", convert = TRUE)
   x2 <- readSource("Navigate", subtype = "SUP_1p5C_Default", convert = TRUE)
@@ -49,7 +58,7 @@ calcNavigateEmissions <- function() {
   
   h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
   
-  qx <- select(x, -c("model", "scenario"))
+  qx <- select(x, -c("model"))
   qx_bu <- qx
   
   ## assign to countries with NA, their H12 region with weights calculated from population
@@ -73,13 +82,13 @@ calcNavigateEmissions <- function() {
   POP <- select(POP, -c("value", "model", "scenario", "X", "RegionCode", "variable", "unit"))
   qx <- left_join(qx, POP, by = c("region", "period"))
   
-  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("period", "variable", "unit"))
+  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("period", "variable", "unit", "scenario"))
   
   qx["value"] <- qx["value"] * qx["weights"]
   
   qx <- select(qx, -c("weights"))
   
-  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit", "scenario")) %>%
     mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
     select(-c("value.x", "value.y"))
   x <- as.quitte(qx) %>% as.magpie()
