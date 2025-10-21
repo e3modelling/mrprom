1ed358999412fe354fa9267fb2eb5dd59f0daccc
Anastasis Giannousakis <giannousakis@e3modelling.com>
2025-10-21 12:58:22 +0300

    bugfix calcStockPC

--- R/calcStockPC.R
+++ R/calcStockPC.R
@@ -172,9 +172,9 @@ helperGetEVShares <- function(mappingEVs, dataIEA_EV, finalY, fillRegions = TRUE
 helperGetNonEVShares <- function(SFC, mappingEVs) {
   shareNonEVs <- calcOutput(type = "IFuelCons2", subtype = "TRANSE", aggregate = FALSE) %>%
     as.quitte() %>%
-    rename(tech = new) %>%
+    rename(tech = ef) %>%
     mutate(tech = paste0("T", tech)) %>%
-    filter(variable == "PC") %>%
+    filter(dsbs == "PC") %>%
     right_join(SFC, by = c("region", "period", "tech")) %>%
     mutate(value = replace_na(value, 0) / SFC) %>%
     filter(
