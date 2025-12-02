20adeae7de91ed8a7c21665f2b89978a2e42e8ce
Michael Madianos <mixalisspa@gmail.com>
2025-12-02 12:42:57 +0200

    Bugfix

--- R/convertIEA_EV.R
+++ R/convertIEA_EV.R
@@ -6,7 +6,7 @@
 #'
 #' @return The "IEA_EV" data with spatial entries for each country.
 #'
-#' @author Fotis Sioutas
+#' @author Fotis Sioutas, Michael Madianos
 #'
 #' @examples
 #' \dontrun{
@@ -15,16 +15,38 @@
 #'
 #' @importFrom dplyr filter %>% pull
 convertIEA_EV <- function(x) {
-  y <- add_columns(x, addnm = setdiff(getISOlist(), getItems(x,1)), dim = 1, fill = NA)
+  y <- add_columns(x, addnm = setdiff(getISOlist(), getItems(x, 1)), dim = 1, fill = 0)
 
   countries <- toolGetMapping("regionmappingH12.csv", where = "madrat") %>%
     filter(RegionCode != "EUR", !CountryCode %in% c("USA", "IND", "CHN")) %>%
     pull(CountryCode)
 
-  y[countries, getYears(y)[getYears(y)>"y2021"], "EV stock share"] <- y["RWRL",getYears(y)[getYears(y)>"y2021"], "EV stock share"]
+  y[countries, getYears(y)[getYears(y) > "y2021"], "EV stock share"] <- y["RWRL", getYears(y)[getYears(y) > "y2021"], "EV stock share"]
   y <- y[as.character(getISOlist()), , ] %>%
     as.quitte() %>%
     interpolate_missing_periods(period = seq(2010, 2030, 1)) %>%
+    mutate(
+      # PHEV is uniformly disaggregated to PHEGSL, PHEGDO
+      powertrain = as.character(powertrain),
+      value = ifelse(powertrain == "PHEV", value / 2, value),
+      powertrain = ifelse(powertrain == "PHEV", "PHEGSL", powertrain)
+    )
+
+  y <- y %>%
+    bind_rows(
+      y %>%
+        filter(powertrain == "PHEGSL") %>%
+        mutate(powertrain = "PHEGDO")
+    ) %>%
+    mutate(
+      powertrain = recode(powertrain,
+        "BEV" = "TELC",
+        "FCEV" = "TH2F",
+        "PHEGSL" = "TPHEGSL",
+        "PHEGDO" = "TPHEGDO"
+      )
+    ) %>%
+    as.quitte() %>%
     as.magpie()
   return(y)
 }
