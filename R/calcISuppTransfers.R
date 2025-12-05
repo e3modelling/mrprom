4d2f9694b5e11631ed94fe80df610fe4f65e3535
Fsioutas <fsioutas@hotmail.com>
2025-12-05 10:46:36 +0200

    add region mean to IEA TRANSFERS

--- R/calcISuppTransfers.R
+++ R/calcISuppTransfers.R
@@ -27,7 +27,7 @@ calcISuppTransfers <- function() {
     separate_rows(IEA, sep = ",") %>%
     rename(product = IEA, variable = OPEN.PROM)
   
-  tes <- readSource("IEA2025", subset = "TRANSFERS") %>%
+  x <- readSource("IEA2025", subset = "TRANSFERS") %>%
     as.quitte() %>%
     filter(unit == "KTOE", product != "TOTAL", !is.na(value)) %>%
     select(-variable) %>%
@@ -38,13 +38,45 @@ calcISuppTransfers <- function() {
     as.quitte() %>%
     as.magpie() %>%
     # FIXME: Proper impute must be done. For now fill with zero.
-    toolCountryFill(fill = 0)
+    toolCountryFill(fill = NA)
   
-  tes[is.na(tes)] <- 0
+  # Converting to quitte object and interpolating periods
+  qx <- as.quitte(x) %>%
+    interpolate_missing_periods(period = getYears(x, as.integer = TRUE), expand.values = TRUE)
+  qx_bu <- qx
   
+  # Assign to countries with NA, their H12 region mean
+  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
+  names(qx) <- sub("region", "CountryCode", names(qx))
+  
+  ## Add h12 mapping to dataset
+  qx <- left_join(qx, h12, by = "CountryCode")
+  
+  ## Add new column containing regional mean value
+  value <- NULL
+  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable"))
+  names(qx) <- sub("CountryCode", "region", names(qx))
+  qx <- select(qx, -c("model", "scenario", "X", "RegionCode"))
+  qx_bu <- select(qx_bu, -c("model", "scenario"))
+  
+  ## Assign the H12 region mean where necessary
+  value.x <- NULL
+  value.y <- NULL
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
+    select(-c("value.x", "value.y"))
+  
+  ## Assign to countries that still have NA, the global mean
+  qx_bu <- qx
+  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("period", "variable"))
+  qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
+    mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
+    select(-c("value.x", "value.y"))
+  
+  # Converting to magpie object
+  x <- as.quitte(qx) %>% as.magpie()
   # Set NA to 0
-  tes[is.na(tes)] <- 0
-  x <- tes
+  x[is.na(x)] <- 0
   
   list(x = x,
        weight = NULL,
