8e98773459c7243f79615c9ef72c7dc26d7605fd
Fsioutas <fsioutas@hotmail.com>
2025-12-09 12:42:07 +0200

    50%GDP,50%POP weights of isupptranfers

--- R/calcISuppTransfers.R
+++ R/calcISuppTransfers.R
@@ -27,6 +27,7 @@ calcISuppTransfers <- function() {
     separate_rows(IEA, sep = ",") %>%
     rename(product = IEA, variable = OPEN.PROM)
   
+  # load TRANSFERS data from IEA
   x <- readSource("IEA2025", subset = "TRANSFERS") %>%
     as.quitte() %>%
     filter(unit == "KTOE", product != "TOTAL", !is.na(value)) %>%
@@ -40,41 +41,124 @@ calcISuppTransfers <- function() {
     # FIXME: Proper impute must be done. For now fill with zero.
     toolCountryFill(fill = NA)
   
-  # Converting to quitte object and interpolating periods
+  # complete incomplete time series
   qx <- as.quitte(x) %>%
     interpolate_missing_periods(period = getYears(x, as.integer = TRUE), expand.values = TRUE)
+  # assign to countries with NA, their H12 region with weights
+  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
+  
+  qx <- select(qx, -c("model", "scenario"))
   qx_bu <- qx
   
-  # Assign to countries with NA, their H12 region mean
-  h12 <- toolGetMapping("regionmappingH12.csv", where = "madrat")
-  names(qx) <- sub("region", "CountryCode", names(qx))
+  ## assign to countries with NA, their H12 region with weights calculated from population and GDP
+  # start with population
+  population <- calcOutput(type = "POP", aggregate = FALSE)
+  population <- as.quitte(population)
   
-  ## Add h12 mapping to dataset
-  qx <- left_join(qx, h12, by = "CountryCode")
+  # compute weights by population
+  names(population) <- sub("region", "CountryCode", names(population))
   
-  ## Add new column containing regional mean value
-  value <- NULL
-  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable"))
-  names(qx) <- sub("CountryCode", "region", names(qx))
-  qx <- select(qx, -c("model", "scenario", "X", "RegionCode"))
-  qx_bu <- select(qx_bu, -c("model", "scenario"))
-  
-  ## Assign the H12 region mean where necessary
+  ## add mapping to population
+  population <- left_join(population, h12, by = "CountryCode")
   value.x <- NULL
   value.y <- NULL
+  weights <- NULL
+  value <- NULL
+  # Compute POP Weights
+  POP <- mutate(population, weights = sum(value, na.rm = TRUE), .by = c("RegionCode", "period"))
+  POP["weights"] <- POP["value"] / POP["weights"]
+  
+  names(POP) <- sub("CountryCode", "region", names(POP))
+  POP <- select(POP, -c("value", "model", "scenario", "X", "variable", "unit"))
+  
+  # Load GDP
+  gdp <- calcOutput(type = "iGDP", aggregate = FALSE)
+  gdp <- as.quitte(gdp)
+  
+  ## add mapping to gdp
+  names(gdp) <- sub("region", "CountryCode", names(gdp))
+  gdp <- left_join(gdp, h12, by = "CountryCode")
+  
+  # Compute GDP Weights
+  GDP <- mutate(gdp, weights_gdp = sum(value, na.rm = TRUE), 
+                .by = c("RegionCode", "period"))
+  
+  GDP["weights_gdp"] <- GDP["value"] / GDP["weights_gdp"]
+  
+  names(GDP) <- sub("CountryCode", "region", names(GDP))
+  
+  GDP <- select(GDP, -c("value", "model", "scenario", "X", "variable", "unit"))
+  
+  # Merge POP + GDP and Create Mixed Weights
+  POP_GDP <- left_join(POP, GDP, by = c("region", "period", "RegionCode"))
+  
+  alpha <- 0.5   # ← change this to control POP vs GDP importance
+  
+  POP_GDP <- mutate(
+    POP_GDP,
+    weights = alpha * weights + (1 - alpha) * weights_gdp
+  )
+  
+  POP_GDP <- select(POP_GDP, -c(weights_gdp))
+  
+  ############
+  qx <- left_join(qx, POP_GDP, by = c("region", "period"))
+  
+  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("RegionCode", "period", "variable", "unit"))
+  
+  qx["value"] <- qx["value"] * qx["weights"]
+  
+  qx <- select(qx, -c("weights"))
+  
   qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
     mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
-    select(-c("value.x", "value.y"))
+    select(-c("value.x", "value.y", "RegionCode"))
   
-  ## Assign to countries that still have NA, the global mean
+  ## assign to countries that still have NA, the global with weights calculated from population and GDP
   qx_bu <- qx
-  qx <- mutate(qx, value = mean(value, na.rm = TRUE), .by = c("period", "variable"))
+  # compute weights by population
+  POP <- mutate(population, weights = sum(value, na.rm = TRUE), .by = c("period"))
+  POP["weights"] <- POP["value"] / POP["weights"]
+  names(POP) <- sub("CountryCode", "region", names(POP))
+  POP <- select(POP, -c("value", "model", "scenario", "X", "RegionCode", "variable", "unit"))
+  
+  # Compute GDP Weights
+  GDP <- mutate(gdp, weights_gdp = sum(value, na.rm = TRUE), 
+                .by = c("period"))
+  
+  GDP["weights_gdp"] <- GDP["value"] / GDP["weights_gdp"]
+  
+  names(GDP) <- sub("CountryCode", "region", names(GDP))
+  
+  GDP <- select(GDP, -c("value", "model", "scenario", "X", "variable", "unit", "RegionCode"))
+  
+  # Merge POP + GDP and Create Mixed Weights
+  POP_GDP <- left_join(POP, GDP, by = c("region", "period"))
+  
+  alpha <- 0.5   # ← change this to control POP vs GDP importance
+  
+  POP_GDP <- mutate(
+    POP_GDP,
+    weights = alpha * weights + (1 - alpha) * weights_gdp
+  )
+  
+  POP_GDP <- select(POP_GDP, -c(weights_gdp))
+  
+  qx <- left_join(qx, POP_GDP, by = c("region", "period"))
+  
+  qx <- mutate(qx, value = sum(value, na.rm = TRUE), .by = c("period", "variable", "unit"))
+  
+  qx["value"] <- qx["value"] * qx["weights"]
+  
+  qx <- select(qx, -c("weights"))
+  
   qx <- left_join(qx_bu, qx, by = c("region", "variable", "period", "unit")) %>%
     mutate(value = ifelse(is.na(value.x), value.y, value.x)) %>%
     select(-c("value.x", "value.y"))
   
   # Converting to magpie object
   x <- as.quitte(qx) %>% as.magpie()
+  
   # Set NA to 0
   x[is.na(x)] <- 0
   
