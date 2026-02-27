3c19e636eff53937fad6658dd46f25dccfd6dc7d
Fsioutas <fsioutas@hotmail.com>
2026-02-27 14:47:36 +0200

    isfc 2023

--- R/calcISFC.R
+++ R/calcISFC.R
@@ -17,9 +17,7 @@
 #' @importFrom quitte as.quitte
 
 calcISFC <- function() {
-  fEndY <- readEvalGlobal(
-    system.file(file.path("extdata", "main.gms"), package = "mrprom")
-  )["fEndY"]
+  fEndY <- 2023
 
   mappingTechnologies <- data.frame(
     category = c(
