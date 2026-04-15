5888743c4742d44152f2072dee42e0377f18b887
MargaritaEfthymiadou <margarita.efthymiadou@ricardo.com>
2026-04-15 11:12:55 +0100

    debugging

--- R/calcResHeatCapFac.R
+++ R/calcResHeatCapFac.R
@@ -22,22 +22,20 @@ calciResHeatCapFac <- function() {
   a <- readSource("HotMaps")
   b <- readSource("EurostatHDD")
 
-  qa <- as.magpie(a)
-  qb <- as.magpie(b)
-
   # --- calculate capacity factors ---  
-  qb_2024 <- qb[, "y2024", ]
-  qb_2010 <- qb[, "y2010", ]
+  b_2024 <- b[, "y2024", ]
+  b_2010 <- b[, "y2010", ]
 
-  ratio <- qb_2024 / qb_2010
+  ratio <- b_2024 / b_2010
   ratio[!is.finite(ratio)] <- NA
 
-  x <- qa * ratio %>%
-    dimSums(dim = setdiff(getSets(x), "region"))
-  
+  ratio <- collapseDim(ratio,2)
+  ratio <- collapseDim(ratio,3)
+  x <- a * ratio 
+ x[is.na(x)] <- 1  #x=1 when NA values
+
   # --- clean data ---  
-  xq <- as.quitte(x) 
-  xq <- xq[, !(names(xq) %in% c("variable1", "unit1"))] %>%
+  xq <- as.quitte(x)  %>%
   mutate(variable = "res_heat_cap_factor")
 
   list(
