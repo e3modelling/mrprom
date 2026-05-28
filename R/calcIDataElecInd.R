91a6d8d9b2dc015d4af00115f41f2220572e5d47
Fsioutas <fsioutas@hotmail.com>
2026-05-28 15:59:40 +0300

    Enhance documentation for calcIDataElecInd and calcIDataElecProd functions; add new calcIDataHeatProd function with detailed descriptions and examples.

--- R/calcIDataElecInd.R
+++ R/calcIDataElecInd.R
@@ -1,6 +1,14 @@
 #' calcIDataElecInd
+#' 
+#' Derives the OPENPROM input parameter {IDataElecInd} using electricity and heat production data.
 #'
-#' Use ENERDATA electricity production data to derive OPENPROM input parameter IDataElecInd
+#' This function computes an electricity intensity indicator based on the ratio of heat production
+#' to electricity production. Heat data is sourced from {IDataHeatProd} and electricity data
+#' from {IDataElecProd} (both ultimately based on IEA-derived datasets).
+#' The indicator is calculated as:
+#' {value = {heat}{elec * 8.598 * 10^{-5}}}
+#' Missing or invalid values are imputed using global averages derived from aggregated world totals.
+#' For aggregation weights are electricity production.
 #'
 #' @return  OPENPROM input data IDataElecInd
 #'
