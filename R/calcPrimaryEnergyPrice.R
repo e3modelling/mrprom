e7eeedbb067d513629d8f870676ada888054d5dd
Fsioutas <fsioutas@hotmail.com>
2026-07-21 16:07:18 +0300

    Refactor calcPrimaryEnergyPrice function to streamline variable initialization and improve readability

--- R/calcPrimaryEnergyPrice.R
+++ R/calcPrimaryEnergyPrice.R
@@ -1,6 +1,5 @@
 #' calcPrimaryEnergyPrice
 #' 
-#' 
 #' @return  OPENPROM input data PrimaryEnergyPrice
 #'
 #' @author Fotis Sioutas
@@ -34,11 +33,40 @@ calcPrimaryEnergyPrice <- function() {
     system.file(file.path("extdata", "main.gms"), package = "mrprom")
   )["fEndY"]
   
+  variable <- EFS
+  period <- fStartHorizon:fEndY
+  unit <- "kUSD2015/toe"
+  
+  df <- tidyr::expand_grid(
+    region = getISOlist(),
+    variable = variable,
+    period = period,
+    unit = unit
+  ) %>%
+    dplyr::mutate(value = NA_real_)
+  
   BMSWAS_Price <- readSource("MAgPIE_BMSWAS_Price")
+  BMSWAS <- BMSWAS_Price[,,"IS.kUSD2015/toe"]
+  getItems(BMSWAS, 3.1) <- "BMSWAS"
+  
+  map <- toolGetMapping("regionmappingOPDEV5.csv", "regional", where = "mrprom")
+  BMSWAS <- toolAggregate(BMSWAS, dim=1, rel = map, from="Region.Code", to="ISO3.Code")
+  
+  CRO <- readSource("IEACrudeOilPrice")
+  CRO <- add_columns(CRO, addnm = "DEU", dim = "region", fill = NA)
+  CRO["DEU",,] <- CRO["WORLD",,]
+  CRO <- toolCountryFill(CRO, fill = NA)
+  CRO[setdiff(getISOlist(),"DEU"),,] <- CRO["DEU",,]
+  getItems(CRO, 3.1) <- "CRO"
+  getItems(CRO, 3.2) <- "kUSD2015/toe"
+  
+  x <- mbind(BMSWAS, CRO)
+  
+  x <- x[,fStartHorizon : fEndY,]
   
   list(x = x,
-       weight = w,
-       unit = "",
+       weight = NULL,
+       unit = "kUSD2015/toe",
        description = "Primary Energy Price")
   
 }
